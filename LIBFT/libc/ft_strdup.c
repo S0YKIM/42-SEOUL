7f9691484b642b17be9537ce91d734aef4817bf6
SOYEON KIM <soyeonkim.9683@gmail.com>
2021-05-14 21:53:38 +0900

    Add files via upload

---
+++ LIBFT/libc/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/06 19:17:36 by sokim             #+#    #+#             */
+/*   Updated: 2021/05/13 20:23:33 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	i;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	str = (char *)malloc(sizeof(*str) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		++i;
+	}
+	str[i] = 0;
+	return (str);
+}
