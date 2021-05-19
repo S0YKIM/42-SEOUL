98060fd2588d48d24a54193f255e398316b1b980
SOYEON KIM <soyeonkim.9683@gmail.com>
2021-05-19 13:36:35 +0900

    Add files via upload

---
+++ LIBFT/BONUS/ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 14:30:47 by sokim             #+#    #+#             */
+/*   Updated: 2021/05/10 18:06:53 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*result;
+	t_list	*temp;
+
+	if (lst == 0 || f == 0)
+		return (0);
+	result = 0;
+	while (lst)
+	{
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&result, del);
+			return (0);
+		}
+		ft_lstadd_back(&result, temp);
+		temp = temp->next;
+		lst = lst->next;
+	}
+	return (result);
+}
