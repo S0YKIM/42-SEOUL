71dd0760fcb915257b6f4f0878ea01beb7c4389f
Soyeon Kim <sokim@c1r7s7.42seoul.kr>
2021-12-11 20:12:25 +0900

    del 이 NULL 이면 바로 종료하지 않고 lst를 free 해준 후에 종료

--- LIBFT/BONUS/ft_lstdelone.c
+++ LIBFT/BONUS/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 13:50:37 by sokim             #+#    #+#             */
-/*   Updated: 2021/05/18 19:44:04 by sokim            ###   ########.fr       */
+/*   Updated: 2021/12/11 17:48:48 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (!lst || !del)
+	if (!lst)
 		return ;
+	if (!del)
+	{
+		free(lst);
+		return ;
+	}
 	del(lst->content);
 	free(lst);
 }
