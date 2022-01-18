a30f585b21a25504a1b4c13a94cfc017818f9f38
Soyeon Kim <sokim@c3r5s7.42seoul.kr>
2022-01-18 16:30:15 +0900

    parse.c: Map Parsing

---
+++ SOLONG/src/parse.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/15 15:34:31 by sokim             #+#    #+#             */
+/*   Updated: 2022/01/18 16:11:21 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	sl_parse_map(t_map *map, char *filename)
+{
+	int		fd;
+	char	*raw;
+	char	*line;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	raw = ft_strdup("");
+	while (get_next_line(fd, &line) > FLAG_EOF)
+	{
+		raw = ft_strjoin(raw, line);
+		raw = ft_strjoin(raw, "\n");
+		map->height++;
+	}
+	map->map = ft_split(raw, '\n');
+	free(raw);
+	map->width = ft_strlen(map->map[0]);
+	map->screen_width = 1600;
+	map->screen_height = 900;
+	return (1);
+}
