744e13df2bf85287acedb2e3c70c25610df6943d
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-01-21 21:13:15 +0900

    Map parsing

--- SOLONG/src/parse.c
+++ SOLONG/src/parse.c
@@ -6,21 +6,31 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 15:34:31 by sokim             #+#    #+#             */
-/*   Updated: 2022/01/19 21:25:00 by sokim            ###   ########.fr       */
+/*   Updated: 2022/01/21 20:48:50 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	sl_parse_map(t_map *map, char *filename)
+static int	sl_open_file(char *filename, t_data *data)
+{
+	int		fd;
+
+	if (!filename)
+		sl_exit_with_message("Invalid file name.", data);
+	fd = open(filename, O_RDONLY, S_IRUSR);
+	if (fd < 0)
+		sl_exit_with_message("Failed to open the file.", data);
+	return (fd);
+}
+
+int	sl_parse_map(t_map *map, char *filename, t_data *data)
 {
 	int		fd;
 	char	*raw;
 	char	*line;
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
+	fd = sl_open_file(filename, data);
 	raw = ft_strdup("");
 	while (get_next_line(fd, &line) > FLAG_EOF)
 	{
