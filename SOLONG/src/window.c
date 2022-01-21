b5fa4dbacf906a7e2699f3b2ee90b199f0635db0
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-01-21 21:13:39 +0900

    Window managing functions

--- SOLONG/src/window.c
+++ SOLONG/src/window.c
@@ -6,22 +6,18 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 19:15:35 by sokim             #+#    #+#             */
-/*   Updated: 2022/01/19 21:26:44 by sokim            ###   ########.fr       */
+/*   Updated: 2022/01/21 20:53:33 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	sl_new_window(t_data *data)
+void	sl_new_window(void *mlx, t_map *map, t_data *data)
 {
-	void	*mlx;
-	void	*win;
-	t_map	map;
-
-	mlx = data->mlx;
-	win = data->win;
-	map = data->map;
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, map.window_width, map.window_height, "so_long");
-	mlx_loop(mlx);
+	if (!mlx)
+		sl_exit_with_message("Failed to initialize.", data);
+	data->win = mlx_new_window(mlx, map->window_width, map->window_height, "so_long");
+	if(!(data->win))
+		sl_exit_with_message("Failed to create new window.", data);
 }
