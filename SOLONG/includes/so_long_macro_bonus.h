e12580be420149f682e2d2250c5ce05a05731ec1
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-01-27 16:08:40 +0900

    Mandatory part completed

---
+++ SOLONG/includes/so_long_macro_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_macro.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 18:49:43 by sokim             #+#    #+#             */
+/*   Updated: 2022/01/26 14:30:07 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_MACRO_H
+# define SO_LONG_MACRO_H
+
+/* 
+ * Key and mouse button press
+ * KEY PRESS: Any key pressed
+ * BUTTON CLOSE: The red X button on the top of windows clicked
+*/
+
+# define KEY_PRESS 2
+# define KEY_ESC 53
+# define KEY_W 13
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_Q 12
+# define KEY_UP 126
+# define KEY_DOWN 125
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+# define BUTTON_CLOSE 17
+
+#endif
