74933047172e73ae35471b3377f3c5329c379081
S0YKIM <soyeonkim.9683@gmail.com>
2022-02-21 02:02:09 +0900

    ADD : Add function to parse argument vectors and put into deque

---
+++ PUSHSWAP/srcs/parse.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 23:46:01 by sokim             #+#    #+#             */
+/*   Updated: 2022/02/20 23:46:01 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static char	**parse_argument_by_space(char *argument)
+{
+	char	**input;
+
+	input = ft_split(argument, ' ');
+	if (!input)
+		return NULL;
+	return (input);
+}
+
+static void	add_integers_into_tmp(char **input, t_deque **tmp)
+{
+	int		i;
+	int		result;
+	t_node	node;
+
+	i = 0;
+	while (input[i])
+	{
+		node.num = ft_atoi(input[i]);
+		result = insertRearLD(*tmp, node);
+		if (!result)
+			exit_with_memory_free("Failed to insert node.", *tmp, NULL, input);
+		i++;
+	}
+}
+
+static int	insert_numbers_into_tmp(int argc, char **argv, t_deque **tmp)
+{
+	char	**input;
+	int		result;
+	int		i;
+
+	i = 0;
+	while (++i < argc)
+	{
+		input = parse_argument_by_space(argv[i]);
+		if (!input)
+			exit_with_memory_free("Failed to parse arguments.", *tmp, NULL, NULL);
+		result = check_validate_integer(input);
+		if (!result)
+			exit_with_memory_free("Not a valid integer.", *tmp, NULL, input);
+		add_integers_into_tmp(input, tmp);
+		free_double_char(input);
+	}
+	return (FT_TRUE);
+}
+
+t_deque	*create_deque_with_arguments(int argc, char **argv)
+{
+	//t_deque	*result;
+	t_deque *tmp;
+
+	tmp = createDeque();
+	if (!tmp)
+		return NULL;
+	insert_numbers_into_tmp(argc, argv, &tmp);
+	return (tmp);
+	//return (result);
+}
