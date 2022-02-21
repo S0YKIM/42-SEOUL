f664d0f8c14850d0dc807df346342582eb60ef50
S0YKIM <soyeonkim.9683@gmail.com>
2022-02-22 01:03:03 +0900

    ADD : Backup main file for displaying deque

---
+++ PUSHSWAP/backup/main_display.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 22:57:33 by sokim             #+#    #+#             */
+/*   Updated: 2022/02/20 22:57:33 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "stdio.h"
+
+// 평가 시에 삭제
+static void	displayDeque(t_deque *pDeque)
+{
+	t_node	*curr;
+	int		i;
+	
+	if (!pDeque)
+		return ;
+	curr = pDeque->pFrontNode;
+	i = 0;
+	while (curr)
+	{
+		printf("Current element count[%i]: %d\n", i, pDeque->currentElementCount);
+		printf("Number: %d\n", curr->num);
+		printf("Index: %d\n", curr->index);
+		printf("Binary number: %s\n", curr->binary);
+		curr = curr->pRLink;
+		i++;
+	}
+	return ;
+}
+
+int	main(int argc, char **argv)
+{
+	t_push_swap	data;
+
+	if (argc < 2)
+		exit(EXIT_FAILURE);
+	create_deque_with_arguments(argc, argv, &data);
+	if (data.a->currentElementCount <= 3)
+		sort_three(&data);
+	else if (data.a->currentElementCount <= 5)
+		sort_five(&data);
+	else
+		sort_many(&data);
+	displayDeque(data.a);
+	displayDeque(data.b);
+	return (0);
+}
