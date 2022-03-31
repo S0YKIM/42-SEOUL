cd36f698fbeb0c770d0c4ef3b31fd27c3525f333
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-03-31 21:31:31 +0900

    ADD : header files directory

---
+++ PHILO/includes/philosophers.h
@@ -0,0 +1,11 @@
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
