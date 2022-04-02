0935271bef4b4e14466e5e3f026a6526743c3f0c
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-02 19:42:22 +0900

    ADD : Macros, structures, functions added

--- PHILO/includes/philosophers.h
+++ PHILO/includes/philosophers.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 16:02:12 by sokim             #+#    #+#             */
+/*   Updated: 2022/04/02 19:41:38 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
@@ -7,5 +19,55 @@
 # include <string.h>
 # include <sys/time.h>
 # include <pthread.h>
+
+# define FALSE 0
+# define TRUE 1
+# define SUCCESS 1
+# define ERROR -1
+
+# define MAX_PHILOS 200
+# define MAX_INT 2147483647
+# define MIN_INT -2147483648
+
+struct	s_philo;
+typedef struct	s_table
+{
+	int				num_of_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_of_must_eat;
+	int				someone_is_dead;
+	long long		start_time;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	*print;
+	t_philo			*philo;
+}	t_table;
+
+typedef struct	s_philo
+{
+	pthread_t	thread;
+	int			id;
+	int			left_fork;
+	int			right_fork;
+	int			eat_cnt;
+	long long	last_eat_time;
+	t_table		*table;
+}	t_philo;
+
+/*
+ * init.c
+*/
+int	init(char **argv, t_table *table);
+
+/*
+ * validate.c
+*/
+int	check_arguments(int argc, char **argv);
+
+/*
+ * utils.c
+*/
+int	ft_atoi(const char *str);
 
 #endif
