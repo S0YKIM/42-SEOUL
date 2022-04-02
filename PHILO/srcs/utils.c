6fa366117e3de804f910421c8cba754a59f4a2ed
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-02 19:44:49 +0900

    ADD : Util functions such as atoi

---
+++ PHILO/srcs/utils.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 18:43:22 by sokim             #+#    #+#             */
+/*   Updated: 2022/04/02 19:00:47 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+static int	is_space(char c)
+{
+	if (c == '\t' || c == '\r' ||
+		c == '\f' || c == '\v' ||
+		c == '\n' || c == ' ')
+		return (TRUE);
+	return (FALSE);
+}
+
+int	ft_atoi(const char *str)
+{
+	long long	num;
+	int			i;
+	int			sign;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (str && is_space(str[i]))
+		++i;
+	if (str[i] == '-')
+		sign *= -1;
+	if (str[i] == '-' || str[i] == '+')
+		++i;
+	while (str[i] && (str[i] >= '0') && (str[i] <= '9'))
+	{
+		num *= 10;
+		num += str[i] - '0';
+		++i;
+	}
+	if (sign * num > MAX_INT || sign * num < MIN_INT)
+		return (ERROR);
+	return (sign * num);
+}
