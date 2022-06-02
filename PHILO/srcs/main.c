80f713a46e40ba6b2acdbdc91b12374c327ba49c
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-02 13:30:19 +0900

    Check if arguments are valid, initialize, and start routine

--- PHILO/srcs/main.c
+++ PHILO/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 16:02:08 by sokim             #+#    #+#             */
-/*   Updated: 2022/05/30 13:05:44 by sokim            ###   ########.fr       */
+/*   Updated: 2022/05/30 14:07:58 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,7 @@ int	main(int argc, char **argv)
 		return (print_error("Invalid arguments."));
 	if (!init(argc, argv, &table))
 		return (print_error("Failed to initialize."));
+	if (!philosophers(&table))
+		return (print_error("Failed to create philo threads."));
 	return (EXIT_SUCCESS);
 }
