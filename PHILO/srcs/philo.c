84bc35d421f8fd393d99c1eddc720427a9d0efa0
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-02 13:32:17 +0900

    Add philosophers() which creates thread and start routine: eating, sleeping, thinking

---
+++ PHILO/srcs/philo.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 14:08:08 by sokim             #+#    #+#             */
+/*   Updated: 2022/06/02 13:27:27 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+static void	eating(t_philo *philo)
+{
+	(void)philo;
+}
+
+static void	sleeping(t_philo *philo)
+{
+	(void)philo;
+}
+
+static void	thinking(t_philo *philo)
+{
+	(void)philo;
+}
+
+static void	*routine(void *void_philo)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)void_philo;
+	if (philo->id % 2 == 0)
+		usleep(philo->table->time_to_eat * 800);
+	while (!(philo->table->someone_is_dead))
+	{
+		eating(philo);
+		sleeping(philo);
+		thinking(philo);
+	}
+	return (NULL);
+}
+
+int	philosophers(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	table->start_time = get_time();
+	while (i < table->num_of_philos)
+	{
+		if (pthread_create(&(table->philo[i].thread), NULL, routine, \
+		(void *)&table->philo[i]))
+			return (ERROR);
+		i++;
+	}
+	return (SUCCESS);
+}
