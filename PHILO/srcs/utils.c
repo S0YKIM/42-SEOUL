aa6313bd662ec941a3aa851eee547296b886afb6
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-02 14:11:25 +0900

    Add print_philo() to utils.c

--- PHILO/srcs/utils.c
+++ PHILO/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 18:43:22 by sokim             #+#    #+#             */
-/*   Updated: 2022/06/02 12:45:43 by sokim            ###   ########.fr       */
+/*   Updated: 2022/06/02 13:50:08 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@ long long	get_time(void)
 	gettimeofday(&time, NULL);
 	ms = (time.tv_sec * 1000) + (time.tv_usec / 1000);
 	return (ms);
+}
+
+void	print_philo(t_philo *philo, long long time, char *msg)
+{
+	printf("%lld ", time - philo->table->start_time);
+	printf("%i ", philo->id);
+	printf("%s\n", msg);
 }
 
 static int	is_space(char c)
