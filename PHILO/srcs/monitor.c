588829e0bf2fe1926ad01a96685ec8ec6c48e753
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-05 14:24:40 +0900

    Add line to lock and unlock monitor mutex in check_exit()

--- PHILO/srcs/monitor.c
+++ PHILO/srcs/monitor.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 15:12:51 by sokim             #+#    #+#             */
-/*   Updated: 2022/06/04 17:50:51 by sokim            ###   ########.fr       */
+/*   Updated: 2022/06/05 14:16:18 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@ static int	check_starve(t_philo *philo, t_table *table)
 {
 	if (get_time() - philo->last_eat_time >= table->time_to_die)
 	{
-		ph_print(philo, get_time(), "died");
 		table->exit = TRUE;
+		pthread_mutex_lock(philo->table->print);
+		printf("%lld ", get_time() - philo->table->start_time);
+		printf("%i ", philo->id);
+		printf("%s\n", "died");
+		pthread_mutex_unlock(philo->table->print);
 		return (TRUE);
 	}
 	return (FALSE);
@@ -34,39 +38,44 @@ static int	check_full(t_table *table)
 			return (FALSE);
 		i++;
 	}
+	table->exit = TRUE;
 	return (TRUE);
 }
 
-void	monitor(t_table *table)
+int	check_exit(t_philo *philo)
 {
-	int	i;
-
-	if (table->num_of_philos == 0)
-		return ;
-	while (!table->exit)
+	pthread_mutex_lock(philo->table->monitor);
+	if (philo->table->exit)
 	{
-		i = 0;
-		while (i < table->num_of_philos)
-		{
-			if (check_starve(&table->philo[i], table))
-				return ;
-			i++;
-		}
-		if (table->num_of_must_eat != 0 && check_full(table))
-			return ;
-		usleep(10);
+		pthread_mutex_unlock(philo->table->monitor);
+		return (TRUE);
 	}
+	if (check_starve(philo, philo->table))
+	{
+		pthread_mutex_unlock(philo->table->monitor);
+		return (TRUE);
+	}
+	if (philo->table->num_of_must_eat != 0 && check_full(philo->table))
+	{
+		pthread_mutex_unlock(philo->table->monitor);
+		return (TRUE);
+	}
+	pthread_mutex_unlock(philo->table->monitor);
+	return (FALSE);
 }
 
 void	end_dining(t_table *table)
 {
 	int	i;
+	int	status;
 
 	i = 0;
 	while (i < table->num_of_philos)
 	{
+		pthread_join(table->philo[i].thread, (void *)&status);
 		pthread_mutex_destroy(&table->fork[i]);
 		i++;
 	}
 	pthread_mutex_destroy(table->print);
+	pthread_mutex_destroy(table->monitor);
 }
