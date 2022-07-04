24cabe940ac2af3338d4cdfc5fb1e0f97c0989dd
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-04 17:20:48 +0900

    fix: Rename function

--- CPP/module-00/ex01/inc/Contact.hpp
+++ CPP/module-00/ex01/inc/Contact.hpp
@@ -6,14 +6,18 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 16:14:41 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/04 16:02:55 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/04 17:17:28 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
+#define	MAX_CONTACT 8
+#define MAX_FIELD_LEN 10
+
 #include <iostream>
+#include <iomanip>
 
 class	Contact {
 private:
@@ -28,7 +32,11 @@ public:
 	Contact();
 	Contact(int index, std::string first_name, std::string last_name, \
 		std::string nickname, std::string phone_number, std::string darkest_secret);
-	void	DisplayContact();
+	
+	std::string	Truncate(std::string original) const;
+	int			GetIndex() const;
+	void		DisplayContact() const;
+	void		DisplaySearchedContact() const;
 };
 
 #endif
