00f3438df6666b77d361ac69a4afd09c4725bd78
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-04 22:15:18 +0900

    feat: Create zombies and destroy them

--- CPP/module-01/ex00/src/main.cpp
+++ CPP/module-01/ex00/src/main.cpp
@@ -6,17 +6,34 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 21:30:59 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/04 21:41:37 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/04 22:10:10 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+std::string	randomName() {
+	std::string names[5] = {"John", "Emma", "Brian", "Ann", "Lee"};
+
+	return (names[rand() % 5]);
+}
+
 int	main(void)
 {
-	Zombie	zombie;
+	Zombie*	zombie1 = newZombie("zombie1");
+	Zombie*	zombie2 = newZombie("zombie2");
+	Zombie*	zombie3 = newZombie("zombie3");
 
-	zombie = Zombie("z");
-	zombie.announce();
+	zombie1->announce();
+	zombie2->announce();
+	zombie3->announce();
+
+	zombie1->~Zombie();
+	zombie2->~Zombie();
+	zombie3->~Zombie();
+
+	randomChump(randomName());
+	randomChump(randomName());
+	randomChump(randomName());
 	return (0);
 }
