43a87e1fae01195a1c355083d9f29cb2ee4f997d
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-04 22:15:42 +0900

    feat: Create zombies by memory allocation

--- CPP/module-01/ex00/src/newZombie.cpp
+++ CPP/module-01/ex00/src/newZombie.cpp
@@ -6,7 +6,13 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 21:28:48 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/04 21:28:49 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/04 21:58:38 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie*	newZombie(std::string name) {
+	Zombie*	zombie = new Zombie(name);
+	return (zombie);
+}
