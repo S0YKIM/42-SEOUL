58436ef9e669ce4f3f839b9d25071bb739aeb6b3
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-04 22:16:06 +0900

    feat: Create zombie instance and announce

--- CPP/module-01/ex00/src/randomChump.cpp
+++ CPP/module-01/ex00/src/randomChump.cpp
@@ -6,7 +6,14 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 21:28:46 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/04 21:28:47 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/04 21:59:59 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+void	randomChump(std::string name) {
+	Zombie	zombie(name);
+
+	zombie.announce();
+}
