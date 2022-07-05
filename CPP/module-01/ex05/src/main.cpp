90ba9a27a752301949bbc4b07cf8df8126b98469
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-06 00:40:54 +0900

    feat: Main function to call four levels of harl implemented

---
+++ CPP/module-01/ex05/src/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 23:20:24 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/06 00:39:10 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void) {
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	return (0);
+}
