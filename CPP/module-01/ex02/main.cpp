95360b3090fb0076cdec583b19438a425f02ae19
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-05 16:07:26 +0900

    feat: Use of pointer and reference

---
+++ CPP/module-01/ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 15:38:50 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/05 16:06:50 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iostream"
+
+int	main(void) {
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+
+	std::cout << "*********************************" << std::endl;
+	std::cout << "&string: \t" << &string << std::endl;
+	std::cout << "stringPTR: \t" << stringPTR << std::endl;
+	std::cout << "&stringREF: \t" << &stringREF << std::endl;
+	std::cout << "*********************************" << std::endl;
+
+	std::cout << "&stringPTR: \t" << &stringPTR << std::endl;
+	std::cout << "*********************************" << std::endl;
+
+	std::cout << "string: \t" << string << std::endl;
+	std::cout << "*stringPTR: \t" << *stringPTR << std::endl;
+	std::cout << "stringREF: \t" << stringREF << std::endl;
+	std::cout << "*********************************" << std::endl;
+}
