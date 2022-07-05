fa682ef56da611fed8537fbd2bc3ab743003cd11
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-05 17:42:44 +0900

    feat: Main function implemented

---
+++ CPP/module-01/ex03/src/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 16:24:48 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/05 17:29:43 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int	main(void) {
+	{
+		std::cout << "***********************************************" << std::endl;
+		Weapon	club = Weapon("crude spiked club");
+		HumanA	bob("Bob", club);
+
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	std::cout << "***********************************************" << std::endl;
+	{
+		Weapon	club = Weapon("crude spiked club");
+		HumanB	jim("Jim");
+
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+			std::cout << "***********************************************" << std::endl;
+	}
+	return (0);
+}
