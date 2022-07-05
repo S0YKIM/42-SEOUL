0512702eff07c4f7e5d5829c6f696d4a66868f01
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-05 16:07:47 +0900

    feat: Use of pointer and reference

--- CPP/module-01/ex02/main.cpp
+++ CPP/module-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 15:38:50 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/05 16:06:50 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/05 16:07:35 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	main(void) {
 	std::string string = "HI THIS IS BRAIN";
 	std::string *stringPTR = &string;
 	std::string &stringREF = string;
-
 
 	std::cout << "*********************************" << std::endl;
 	std::cout << "&string: \t" << &string << std::endl;
