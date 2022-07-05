1e80fe66c995a741617bf41c213bd4350b29c64b
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-05 15:37:32 +0900

    feat: Function to create several zombies by memory allocation implemented

---
+++ CPP/module-01/ex01/src/zombieHorde.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 14:06:40 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/05 14:38:26 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int N, std::string name) {
+	Zombie*	horde = new Zombie[N];
+
+	for (int i = 0; i < N; i++) {
+		horde[i].setName(name);
+	}
+	return (horde);
+}
