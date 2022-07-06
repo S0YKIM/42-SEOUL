623c969e2ae8b32fa32c90a66a8fff5d772d7e49
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-06 12:05:39 +0900

    feat: Headerfile of ex06

---
+++ CPP/module-01/ex06/inc/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 11:39:13 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/06 11:39:17 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl {
+private:
+	void	debug();
+	void	info();
+	void	warning();
+	void	error();
+
+public:
+	Harl();
+	~Harl();
+
+	void	complain(std::string level);
+};
+
+#endif
