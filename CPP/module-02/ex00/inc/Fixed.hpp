3823f5909ea8c9c359ddb8e37956a0062e31710c
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-06 19:50:31 +0900

    feat: Headerfile following Orthodox Canonical Form

---
+++ CPP/module-02/ex00/inc/Fixed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 14:18:46 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/06 19:31:46 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class	Fixed {
+private:
+	int					fixedPointValue;
+	static const int	fractionalBits;
+
+public:
+	Fixed();
+	Fixed(const Fixed &origin);
+	Fixed &operator=(const Fixed &origin);
+	~Fixed();
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+};
+
+#endif
