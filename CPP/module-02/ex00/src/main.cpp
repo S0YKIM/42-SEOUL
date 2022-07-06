f68771dc438b9c01879864848dd9c6456a431a2d
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-06 19:49:47 +0900

    feat: Main function implemented

---
+++ CPP/module-02/ex00/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 14:18:43 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/06 19:34:58 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void) {
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (0);
+}
