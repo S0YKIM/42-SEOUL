95bbe1c1d79c334cfc2c3309d82de006be8dda30
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-07 10:39:30 +0900

    feat: Main function of ex02

---
+++ CPP/module-02/ex02/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 09:37:07 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/07 10:20:26 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void) {
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max(a, b) << std::endl;
+
+	return (0);
+}
