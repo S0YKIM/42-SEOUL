7a81303b69c974a94e928cd2c1addaeedc484d68
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-08 10:44:55 +0900

    add: FragTrap class from ex02

---
+++ CPP/module-03/ex03/inc/FragTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 16:57:45 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/08 09:44:13 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class	FragTrap : public ClapTrap {
+	public:
+		FragTrap();
+		FragTrap(std::string name);
+		FragTrap(const FragTrap &origin);
+		FragTrap &operator=(const FragTrap &origin);
+		~FragTrap();
+
+		void highFivesGuys();
+};
+
+#endif
