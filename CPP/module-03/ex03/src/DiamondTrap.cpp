c6881c1f1e10e0d8ca10111258f3f9cabe6c34e8
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-08 10:44:29 +0900

    feat: DiamondTrap class implemented

---
+++ CPP/module-03/ex03/src/DiamondTrap.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 09:54:52 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/08 10:15:39 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap() : ClapTrap("Default_clap_name"), DiamondTrap(), FragTrap() {
+	std::cout << "DiamondTrap default constructor called." << std::endl;
+	this->name = "Default";
+	this->maxHitPoints = this->FragTrap::maxHitPoints;
+	this->hitPoints = this->FragTrap::hitPoints;
+	this->energyPoints = this->DiamondTrap::energyPoints;
+	this->attackDamage = this->FragTrap::attackDamage;
+}
+
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), DiamondTrap(name), FragTrap(name) {
+	std::cout << "DiamondTrap " << name << " constructed." << std::endl;
+	this->name = name;
+	this->maxHitPoints = this->FragTrap::maxHitPoints;
+	this->hitPoints = this->FragTrap::hitPoints;
+	this->energyPoints = this->DiamondTrap::energyPoints;
+	this->attackDamage = this->FragTrap::attackDamage;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &origin) {
+	std::cout << "DiamondTrap " << origin.name << " copied." << std::endl;
+	*this = origin;
+}
+
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &origin) {
+	std::cout << "DiamondTrap " << origin.name << " assigned." << std::endl;
+	if (this == &origin)
+		return (*this);
+	this->name = origin.name;
+	this->maxHitPoints = origin.maxHitPoints;
+	this->hitPoints = origin.hitPoints;
+	this->energyPoints = origin.energyPoints;
+	this->attackDamage = origin.attackDamage;
+	ClapTrap::operator=(origin);
+	return (*this);
+}
+
+DiamondTrap:: ~DiamondTrap() {
+	std::cout << "DiamondTrap " << this->name << " destructed." << std::endl;
+}
+
+void	DiamondTrap::attack(const std::string &target) {
+	this->ScavTrap::attack(target);
+}
+
+void	DiamondTrap::whoAmI() {
+	std::cout << "I'm " << this->name << " and also " << this->ClapTrap::name << std::endl;
+}
