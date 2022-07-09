85966bb7370ef1688aa75d99a4fc611843d47d35
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-09 15:49:45 +0900

    feat: Pure virtual function getBrain() added to Animal class

---
+++ CPP/module-04/ex01/inc/Animal.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 17:12:08 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/09 15:14:30 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+#include "Brain.hpp"
+
+class Animal {
+protected:
+	std::string	type_;
+
+public:
+	Animal();
+	Animal(const Animal &origin);
+	Animal &operator=(const Animal &origin);
+	virtual ~Animal();
+
+	std::string		getType() const;
+	void			setType(std::string type);
+
+	virtual void	makeSound() const;
+	virtual Brain*	getBrain() const = 0;
+};
+
+#endif
