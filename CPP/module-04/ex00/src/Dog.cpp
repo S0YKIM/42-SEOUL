89f217ebc9abf2c0f89988bd434d9496964ae3f4
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-09 13:45:26 +0900

    feat: Dog class definition implemented

--- CPP/module-04/ex00/src/Dog.cpp
+++ CPP/module-04/ex00/src/Dog.cpp
@@ -6,7 +6,33 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 17:12:01 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/08 17:12:01 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/09 13:40:59 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Dog.hpp"
+
+Dog::Dog() : Animal() {
+	std::cout << "Dog default constructor called." << std::endl;
+	setType("Dog");
+}
+
+Dog::Dog(const Dog &origin) {
+	std::cout << "Dog copy constructor called." << std::endl;
+	*this = origin;
+}
+
+Dog &Dog::operator=(const Dog &origin) {
+	if (this == &origin)
+		return (*this);
+	setType(origin.getType());
+	return (*this);
+}
+
+Dog::~Dog() {
+	std::cout << "Dog destructor called." << std::endl;
+}
+
+void	Dog::makeSound() const {
+	std::cout << "Bark!" << std::endl;
+}
