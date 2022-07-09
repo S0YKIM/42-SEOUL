efd02cd53788b263c7347ac0bee9b8861c64e487
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-09 14:23:38 +0900

    feat: WrongCat class inheriting WrongAnimal implemented

---
+++ CPP/module-04/ex00/src/WrongCat.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/09 14:08:07 by sokim             #+#    #+#             */
+/*   Updated: 2022/07/09 14:13:10 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() : WrongAnimal() {
+	std::cout << "WrongCat default constructor called." << std::endl;
+	setType("WrongCat");
+}
+
+WrongCat::WrongCat(const WrongCat &origin) {
+	std::cout << "WrongCat copy constructor called." << std::endl;
+	*this = origin;
+}
+
+WrongCat &WrongCat::operator=(const WrongCat &origin) {
+	if (this == &origin)
+		return (*this);
+	setType(origin.getType());
+	return (*this);
+}
+
+WrongCat::~WrongCat() {
+	std::cout << "WrongCat destructor called." << std::endl;
+}
+
+void	WrongCat::makeSound() const {
+	std::cout << "Meow?" << std::endl;
+}
