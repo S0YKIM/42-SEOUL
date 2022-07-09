db1e8d738251f5ddbe68c4d834064d7824752519
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-09 13:44:03 +0900

    feat: Dog class which inherits from Animal class implemented

--- CPP/module-04/ex00/inc/Dog.hpp
+++ CPP/module-04/ex00/inc/Dog.hpp
@@ -6,7 +6,25 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 17:12:13 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/08 17:12:13 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/09 13:06:52 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal {
+public:
+	Dog();
+	Dog(std::string type);
+	Dog(const Dog &origin);
+	Dog &operator=(const Dog &origin);
+	~Dog();
+
+	void	makeSound() const;
+};
+
+#endif
