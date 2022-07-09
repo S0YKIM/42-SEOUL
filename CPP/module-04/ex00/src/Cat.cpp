b49e6f57d036370f68e92652441130ec0c3c3f5e
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-09 13:45:18 +0900

    feat: Cat class definition implemented

--- CPP/module-04/ex00/src/Cat.cpp
+++ CPP/module-04/ex00/src/Cat.cpp
@@ -6,7 +6,33 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 17:11:59 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/08 17:11:59 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/09 13:40:30 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Cat.hpp"
+
+Cat::Cat() : Animal() {
+	std::cout << "Cat default constructor called." << std::endl;
+	setType("Cat");
+}
+
+Cat::Cat(const Cat &origin) {
+	std::cout << "Cat copy constructor called." << std::endl;
+	*this = origin;
+}
+
+Cat &Cat::operator=(const Cat &origin) {
+	if (this == &origin)
+		return (*this);
+	setType(origin.getType());
+	return (*this);
+}
+
+Cat::~Cat() {
+	std::cout << "Cat destructor called." << std::endl;
+}
+
+void	Cat::makeSound() const {
+	std::cout << "Meow!" << std::endl;
+}
