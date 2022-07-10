4105b79ce473fe2e84b4b6da6741aebd73a4b5fb
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-10 19:42:54 +0900

    refactor: Move try/catch from main to bureaucrat

--- CPP/module-05/ex00/src/main.cpp
+++ CPP/module-05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 11:26:14 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/10 17:10:57 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/10 19:39:35 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,46 +16,19 @@ int	main(void) {
 	std::cout << "******************************************" << std::endl;
 	Bureaucrat	a("Anna", 1);
 	std::cout << a << std::endl;
-	try {
-		a.incrementGrade();
-		std::cout << "This will not be printed because incrementing should fail." << std::endl;
-	}
-	catch (const std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << "******************************************" << std::endl << std::endl;
-
-	std::cout << "******************************************" << std::endl;
-	try {
-		a.decrementGrade();
-		std::cout << a << std::endl;
-	}
-	catch (const std::exception &e) {
-		std::cout << "This will not be printed because decrementing should succeed." << std::endl;
-		std::cerr << e.what() << std::endl;
-	}
+	a.incrementGrade();
+	a.decrementGrade();
 	std::cout << "******************************************" << std::endl << std::endl;
 
 	std::cout << "******************************************" << std::endl;
 	Bureaucrat	b("Brian", 150);
 	std::cout << b << std::endl;
-	try {
-		b.decrementGrade();
-		std::cout << "This will not be printed because decrementing should fail." << std::endl;
-	}
-	catch (const std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
+	b.decrementGrade();
+	b.incrementGrade();
 	std::cout << "******************************************" << std::endl << std::endl;
 
 	std::cout << "******************************************" << std::endl;
-	try {
-		b.incrementGrade();
-		std::cout << b << std::endl;
-	}
-	catch (const std::exception &e) {
-		std::cout << "This will not be printed because incrementing should succeed." << std::endl;
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << "******************************************" << std::endl << std::endl;
+	Bureaucrat	wrong1("Wrong1", 0);
+	Bureaucrat	wrong2("Wrong2", 151);
+	std::cout << "*****************************************" << std::endl << std::endl;
 }
