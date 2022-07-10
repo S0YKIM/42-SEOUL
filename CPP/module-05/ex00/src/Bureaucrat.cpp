84d42d84637160c2fcb9f065529abb8025971cd1
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-10 20:14:39 +0900

    refactor: Move try/catch from Bureaucrat.cpp to main

--- CPP/module-05/ex00/src/Bureaucrat.cpp
+++ CPP/module-05/ex00/src/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 11:26:17 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/10 19:40:54 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/10 20:09:42 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,11 @@ Bureaucrat::Bureaucrat() : name_("Default"), grade_(LOWEST_GRADE) {
 }
 
 Bureaucrat::Bureaucrat(const std::string &name, int grade) : name_(name), grade_(grade) {
-	try {
-		if (grade < HIGHEST_GRADE)
-			throw Bureaucrat::GradeTooHighException();
-		else if (grade > LOWEST_GRADE)
-			throw Bureaucrat::GradeTooLowException();
-	}
-	catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
+
+	if (grade < HIGHEST_GRADE)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > LOWEST_GRADE)
+		throw Bureaucrat::GradeTooLowException();
 }
 
 Bureaucrat::Bureaucrat(Bureaucrat const &origin) {
@@ -51,25 +47,15 @@ int	Bureaucrat::getGrade() const {
 }
 
 void	Bureaucrat::incrementGrade() {
-	try {
-		if (grade_ - 1 < HIGHEST_GRADE)
-			throw Bureaucrat::GradeTooHighException();
-		grade_ -= 1;
-	}
-	catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
+	if (grade_ - 1 < HIGHEST_GRADE)
+		throw Bureaucrat::GradeTooHighException();
+	grade_ -= 1;
 }
 
 void	Bureaucrat::decrementGrade() {
-	try {
-		if (grade_ + 1 > LOWEST_GRADE)
-			throw Bureaucrat::GradeTooLowException();
-		grade_ += 1;
-	}
-	catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
+	if (grade_ + 1 > LOWEST_GRADE)
+		throw Bureaucrat::GradeTooLowException();
+	grade_ += 1;
 }
 
 const char	*Bureaucrat::GradeTooHighException::what() const throw() {
--- CPP/module-05/ex00/src/main.cpp
+++ CPP/module-05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 11:26:14 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/10 19:44:42 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/10 20:13:10 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,70 @@
 
 int	main(void) {
 	std::cout << "******************************************" << std::endl;
+	std::cout << "*                TEST 1                  *" << std::endl;
+	std::cout << "******************************************" << std::endl;
 	Bureaucrat	a("Anna", 1);
 	std::cout << a << std::endl;
-	a.incrementGrade();
-	a.decrementGrade();
-	std::cout << "******************************************" << std::endl << std::endl;
+	try {
+		a.incrementGrade();
+		std::cout << "This will not be printed because incrementing should fail." << std::endl;
+	}
+	catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl << std::endl;
 
+	std::cout << "******************************************" << std::endl;
+	std::cout << "*                TEST 2                  *" << std::endl;
+	std::cout << "******************************************" << std::endl;
+	try {
+		a.decrementGrade();
+		std::cout << a << std::endl;
+	}
+	catch (const std::exception &e) {
+		std::cout << "This will not be printed because decrementing should succeed." << std::endl;
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl << std::endl;
+
+	std::cout << "******************************************" << std::endl;
+	std::cout << "*                TEST 3                  *" << std::endl;
 	std::cout << "******************************************" << std::endl;
 	Bureaucrat	b("Brian", 150);
 	std::cout << b << std::endl;
-	b.decrementGrade();
-	b.incrementGrade();
+	try {
+		b.decrementGrade();
+		std::cout << "This will not be printed because decrementing should fail." << std::endl;
+	}
+	catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	std::cout << "******************************************" << std::endl << std::endl;
 
 	std::cout << "******************************************" << std::endl;
-	Bureaucrat	wrong1("Wrong1", 0);
-	Bureaucrat	wrong2("Wrong2", 151);
-	std::cout << "*****************************************" << std::endl << std::endl;
+	std::cout << "*                TEST 4                  *" << std::endl;
+	std::cout << "******************************************" << std::endl;
+	try {
+		b.incrementGrade();
+		std::cout << b << std::endl;
+	}
+	catch (const std::exception &e) {
+		std::cout << "This will not be printed because incrementing should succeed." << std::endl;
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl << std::endl;
+
+	std::cout << "******************************************" << std::endl;
+	std::cout << "*                TEST 5                  *" << std::endl;
+	std::cout << "******************************************" << std::endl;
+	try {
+		Bureaucrat	wrong("Wrong", 0);
+		std::cout << "This will not be printed because grade is not valid." << std::endl;
+	}
+	catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl << std::endl;
 
 	return (0);
 }
