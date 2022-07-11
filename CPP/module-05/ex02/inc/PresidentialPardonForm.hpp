fb36d65a3400a02452573ff6f1fd8d0e7ba013ce
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-11 12:33:11 +0900

    feat: PresidentialPardonForm class implemented

--- CPP/module-05/ex02/inc/PresidentialPardonForm.hpp
+++ CPP/module-05/ex02/inc/PresidentialPardonForm.hpp
@@ -6,7 +6,32 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 19:53:53 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/10 19:53:53 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/11 11:33:41 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef P_FORM_HPP
+#define P_FORM_HPP
+
+#include "Form.hpp"
+
+#define PRESIDENTIAL_SIGN_GRADE 25
+#define PRESIDENTIAL_EXEC_GRADE 5
+
+class PresidentialPardonForm : public Form {
+private:
+	std::string	target_;
+
+public:
+	PresidentialPardonForm();
+	PresidentialPardonForm(const std::string &target);
+	PresidentialPardonForm(const PresidentialPardonForm &origin);
+	PresidentialPardonForm &operator=(const PresidentialPardonForm &origin);
+	~PresidentialPardonForm();
+
+	std::string	getTarget() const;
+
+	void		execute(Bureaucrat const &executor) const;
+};
+
+#endif
