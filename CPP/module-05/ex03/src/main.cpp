b82bcf7fa9d1a52d30ca715277f3457c6ad2ff2f
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-11 15:45:24 +0900

    test: Test code for intern class

--- CPP/module-05/ex03/src/main.cpp
+++ CPP/module-05/ex03/src/main.cpp
@@ -6,57 +6,72 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 11:26:14 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/11 12:21:50 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/11 13:06:54 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
 int	main(void) {
 	Bureaucrat				a("Anna", 1);
-	Bureaucrat				b("Brian", 70);
-	Bureaucrat				c("Charlie", 150);
-	ShrubberyCreationForm	shruberry("city");
-	RobotomyRequestForm		robotomy("robot");
-	PresidentialPardonForm	president("president");
+	Intern					intern;
 	Form					*form;
 
 	std::cout << "******************************************" << std::endl;
 	std::cout << "*                TEST 1                  *" << std::endl;
 	std::cout << "******************************************" << std::endl;
 	
-	form = &shruberry;
-	std::cout << a << std::endl;
-	std::cout << *form << std::endl;
-	a.executeForm(*form); // Fail
-	a.signForm(*form); // Success
-	a.executeForm(*form); // Success
+	try {
+		form = intern.makeForm("unknown", "target");
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 	std::cout << std::endl << std::endl;
 
 	std::cout << "******************************************" << std::endl;
 	std::cout << "*                TEST 2                  *" << std::endl;
 	std::cout << "******************************************" << std::endl;
 	
-	form = &robotomy;
-	std::cout << b << std::endl;
+	try {
+		form = intern.makeForm("shrubbery creation", "city");
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 	std::cout << *form << std::endl;
-	b.signForm(*form); // Success
-	b.executeForm(*form); // Fail
-	a.executeForm(*form); // Success
+	a.signForm(*form);
+	a.executeForm(*form);
 	std::cout << std::endl << std::endl;
 
 	std::cout << "******************************************" << std::endl;
 	std::cout << "*                TEST 3                  *" << std::endl;
 	std::cout << "******************************************" << std::endl;
 
-	form = &president;
-	std::cout << c << std::endl;
+	try {
+		form = intern.makeForm("robotomy request", "robot");
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 	std::cout << *form << std::endl;
-	c.signForm(*form); // Fail
-	a.signForm(*form); // Success
-	a.executeForm(*form); // Success
+	a.signForm(*form);
+	a.executeForm(*form);
+	std::cout << std::endl << std::endl;
+
+	std::cout << "******************************************" << std::endl;
+	std::cout << "*                TEST 4                  *" << std::endl;
+	std::cout << "******************************************" << std::endl;
+
+	try {
+		form = intern.makeForm("presidential pardon", "president");
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << *form << std::endl;
+	a.signForm(*form);
+	a.executeForm(*form);
 	std::cout << std::endl << std::endl;
 
 	return (0);
