e322097f91c6ac137d4f7d6abcc37deeed585978
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-11 20:32:57 +0900

    add: Convert class work in process

--- CPP/module-06/ex00/src/Convert.cpp
+++ CPP/module-06/ex00/src/Convert.cpp
@@ -6,18 +6,24 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 17:38:04 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/11 17:45:43 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/11 20:32:35 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Convert.hpp"
 
-Convert::Convert() {}
+Convert::Convert() : {}
+//멤버 변수 추가
 
 Convert(const Convert &origin) {
 	*this = origin;
 }
 
 Convert &operator=(const Convert &origin) {
-	
+	if (this == &origin)
+		return (*this);
+	// 멤버 변수 복사
+	return (*this);
 }
+
+Convert::~Convert() {}
