6cc499632d6993cdd75e814be0288552d09906a0
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-11 12:33:29 +0900

    feat: RobotomyRequestForm class implemented

--- CPP/module-05/ex02/inc/RobotomyRequestForm.hpp
+++ CPP/module-05/ex02/inc/RobotomyRequestForm.hpp
@@ -6,7 +6,32 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 19:54:08 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/10 19:54:08 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/11 11:51:37 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef R_FORM_HPP
+#define R_FORM_HPP
+
+#include "Form.hpp"
+
+#define ROBOTOMY_SIGN_GRADE 72
+#define ROBOTOMY_EXEC_GRADE 45
+
+class RobotomyRequestForm : public Form {
+private:
+	std::string	target_;
+
+public:
+	RobotomyRequestForm();
+	RobotomyRequestForm(const std::string &target);
+	RobotomyRequestForm(const RobotomyRequestForm &origin);
+	RobotomyRequestForm &operator=(const RobotomyRequestForm& origin);
+	~RobotomyRequestForm();
+
+	std::string	getTarget() const;
+
+	void		execute(Bureaucrat const &executor) const;
+};
+
+#endif
--- CPP/module-05/ex02/src/RobotomyRequestForm.cpp
+++ CPP/module-05/ex02/src/RobotomyRequestForm.cpp
@@ -6,7 +6,39 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 19:54:04 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/10 19:54:05 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/11 12:13:32 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : Form("RobotomyRequestForm", ROBOTOMY_SIGN_GRADE, ROBOTOMY_EXEC_GRADE), target_("Default") {}
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string &target) : Form("RobotomyRequestForm", ROBOTOMY_SIGN_GRADE, ROBOTOMY_EXEC_GRADE), target_(target) {}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &origin) : Form(origin), target_(origin.getTarget()) {}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &origin) {
+	Form::operator=(origin);
+	target_ = origin.getTarget();
+	return (*this);
+}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+std::string	RobotomyRequestForm::getTarget() const {
+	return (target_);
+}
+
+void	RobotomyRequestForm::execute(Bureaucrat const &executor) const {
+	if (!getIsSigned())
+		throw NotSignedException();
+	else if (executor.getGrade() > getGradeToExecute())
+		throw GradeTooLowException();
+
+	std::cout << "* Drrrrrrrrrrrrrrrriiiiiiillllllllllll... *" << std::endl;
+	if (std::rand() % 2)
+		std::cout << target_ << " failed to be robotomized!" << std::endl;
+	else
+		std::cout << target_ << " has been successfully robotomized!" << std::endl;
+}
