a1d86b7d82ba888c77acb067ba771668155345a4
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-11 16:20:21 +0900

    refactor: Refactor main function

--- CPP/module-05/ex03/src/main.cpp
+++ CPP/module-05/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 11:26:14 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/11 13:06:54 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/11 16:18:52 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,64 +15,78 @@
 int	main(void) {
 	Bureaucrat				a("Anna", 1);
 	Intern					intern;
-	Form					*form;
 
-	std::cout << "******************************************" << std::endl;
-	std::cout << "*                TEST 1                  *" << std::endl;
-	std::cout << "******************************************" << std::endl;
-	
-	try {
-		form = intern.makeForm("unknown", "target");
+	{
+		std::cout << "******************************************" << std::endl;
+		std::cout << "*                TEST 1                  *" << std::endl;
+		std::cout << "******************************************" << std::endl;
+		
+		Form	*form;
+		try {
+			form = intern.makeForm("unknown", "target");
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		delete form;
+		std::cout << std::endl << std::endl;
 	}
-	catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl << std::endl;
 
-	std::cout << "******************************************" << std::endl;
-	std::cout << "*                TEST 2                  *" << std::endl;
-	std::cout << "******************************************" << std::endl;
-	
-	try {
-		form = intern.makeForm("shrubbery creation", "city");
+	{
+		std::cout << "******************************************" << std::endl;
+		std::cout << "*                TEST 2                  *" << std::endl;
+		std::cout << "******************************************" << std::endl;
+		
+		Form	*form;
+		try {
+			form = intern.makeForm("shrubbery creation", "city");
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << *form << std::endl;
+		a.signForm(*form);
+		a.executeForm(*form);
+		delete form;
+		std::cout << std::endl << std::endl;
 	}
-	catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << *form << std::endl;
-	a.signForm(*form);
-	a.executeForm(*form);
-	std::cout << std::endl << std::endl;
 
-	std::cout << "******************************************" << std::endl;
-	std::cout << "*                TEST 3                  *" << std::endl;
-	std::cout << "******************************************" << std::endl;
+	{
+		std::cout << "******************************************" << std::endl;
+		std::cout << "*                TEST 3                  *" << std::endl;
+		std::cout << "******************************************" << std::endl;
 
-	try {
-		form = intern.makeForm("robotomy request", "robot");
+		Form	*form;
+		try {
+			form = intern.makeForm("robotomy request", "robot");
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << *form << std::endl;
+		a.signForm(*form);
+		a.executeForm(*form);
+		delete form;
+		std::cout << std::endl << std::endl;
 	}
-	catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << *form << std::endl;
-	a.signForm(*form);
-	a.executeForm(*form);
-	std::cout << std::endl << std::endl;
 
-	std::cout << "******************************************" << std::endl;
-	std::cout << "*                TEST 4                  *" << std::endl;
-	std::cout << "******************************************" << std::endl;
+	{
+		std::cout << "******************************************" << std::endl;
+		std::cout << "*                TEST 4                  *" << std::endl;
+		std::cout << "******************************************" << std::endl;
 
-	try {
-		form = intern.makeForm("presidential pardon", "president");
+		Form	*form;
+		try {
+			form = intern.makeForm("presidential pardon", "president");
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << *form << std::endl;
+		a.signForm(*form);
+		a.executeForm(*form);
+		delete form;
+		std::cout << std::endl << std::endl;
 	}
-	catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << *form << std::endl;
-	a.signForm(*form);
-	a.executeForm(*form);
-	std::cout << std::endl << std::endl;
-
 	return (0);
 }
