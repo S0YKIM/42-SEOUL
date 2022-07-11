3d0d4e1209f731552a1d8cc43ac2855a1af96ccb
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-11 12:31:56 +0900

    test: Test code to let bureaucrats sign or execute forms

--- CPP/module-05/ex02/src/main.cpp
+++ CPP/module-05/ex02/src/main.cpp
@@ -6,56 +6,57 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 11:26:14 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/10 20:25:58 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/11 12:21:50 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Form.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int	main(void) {
-	Bureaucrat	a("Anna", 1);
-	Bureaucrat	b("Brian", 150);
-	Form		f("Form1", 1, 1);
+	Bureaucrat				a("Anna", 1);
+	Bureaucrat				b("Brian", 70);
+	Bureaucrat				c("Charlie", 150);
+	ShrubberyCreationForm	shruberry("city");
+	RobotomyRequestForm		robotomy("robot");
+	PresidentialPardonForm	president("president");
+	Form					*form;
 
 	std::cout << "******************************************" << std::endl;
 	std::cout << "*                TEST 1                  *" << std::endl;
 	std::cout << "******************************************" << std::endl;
+	
+	form = &shruberry;
 	std::cout << a << std::endl;
-	try {
-		std::cout << f << std::endl;
-		a.signForm(f);
-		std::cout << f << std::endl;
-	}
-	catch (const std::exception &e) {
-		std::cout << "This will not be printed because all of above may succeed." << std::endl;
-		std::cerr << e.what() << std::endl;
-	}
+	std::cout << *form << std::endl;
+	a.executeForm(*form); // Fail
+	a.signForm(*form); // Success
+	a.executeForm(*form); // Success
 	std::cout << std::endl << std::endl;
 
 	std::cout << "******************************************" << std::endl;
 	std::cout << "*                TEST 2                  *" << std::endl;
 	std::cout << "******************************************" << std::endl;
-	try {
-		a.decrementGrade();
-		std::cout << a << std::endl;
-		a.signForm(f);
-	}
-	catch (const std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
+	
+	form = &robotomy;
+	std::cout << b << std::endl;
+	std::cout << *form << std::endl;
+	b.signForm(*form); // Success
+	b.executeForm(*form); // Fail
+	a.executeForm(*form); // Success
 	std::cout << std::endl << std::endl;
 
 	std::cout << "******************************************" << std::endl;
 	std::cout << "*                TEST 3                  *" << std::endl;
 	std::cout << "******************************************" << std::endl;
-	std::cout << b << std::endl;
-	try {
-		Form	wrongForm("Wrong", 0, 0);
-		std::cout << "This will not be printed because grade is invalid." << std::endl;
-	}
-	catch (const std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
+
+	form = &president;
+	std::cout << c << std::endl;
+	std::cout << *form << std::endl;
+	c.signForm(*form); // Fail
+	a.signForm(*form); // Success
+	a.executeForm(*form); // Success
 	std::cout << std::endl << std::endl;
 
 	return (0);
