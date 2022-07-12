58820ced507b0f8e687098a1c3618517de032f31
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-12 15:12:39 +0900

    feat: Main function implemented

--- CPP/module-06/ex00/src/main.cpp
+++ CPP/module-06/ex00/src/main.cpp
@@ -6,7 +6,22 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 17:38:02 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/11 17:38:03 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/12 15:11:08 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Convert.hpp"
+
+int	main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cerr << "Wrong input! Please try again." << std::endl;
+		std::cout << "Usage: ./convert [argument]" << std::endl;
+		std::cout << "Type of arguments: char, int, float, double" << std::endl;
+		return (1);
+	}
+
+	Convert	converter(argv[1]);
+	converter.printAll();
+
+	return (0);
+}
