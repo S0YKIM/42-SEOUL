95c5ef34a73a4743bb67d61820c73f660770ad3e
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-13 15:41:40 +0900

    refactor: Change f() to (*f)() (implicit dereference to explicit reference)

--- CPP/module-07/ex01/inc/iter.hpp
+++ CPP/module-07/ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 11:01:30 by sokim             #+#    #+#             */
-/*   Updated: 2022/07/13 11:18:15 by sokim            ###   ########.fr       */
+/*   Updated: 2022/07/13 15:40:22 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@
 template <typename T>
 void	iter(T *array, size_t len, void (*f)(T)) {
 	for (size_t i = 0; i < len; i++)
-		f(array[i]);
+		(*f)(array[i]);
 }
 
 template <typename T>
 void	iter(T *array, size_t len, void (*f)(T &)) {
 	for (size_t i = 0; i < len; i++)
-		f(array[i]);
+		(*f)(array[i]);
 }
 
 template <typename T>
 void	iter(T *array, size_t len, void (*f)(const T &)) {
 	for (size_t i = 0; i < len; i++)
-		f(array[i]);
+		(*f)(array[i]);
 }
 
 #endif
