43ef8dcabe47825bf676e2d677410123d0bf55d5
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-15 17:32:48 +0900

    feat: Set default value of template argument Container to ft::vector<T>

--- CONTAINERS/inc/stack.hpp
+++ CONTAINERS/inc/stack.hpp
@@ -6,11 +6,20 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:49:23 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/14 19:49:38 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/15 17:28:41 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP
 # define STACK_HPP
+
+# include <vector.hpp>
+
+namespace ft {
+template <typename T, typename Container = ft::vector<T>>
+class stack {
+
+};
+}
 
 #endif
