a6b9e6faf54c9b186ca8dd8190ef09470ad553cd
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-15 17:31:51 +0900

    fix: Set default value of template argument Alloc to std::allocator<T>

--- CONTAINERS/inc/vector.hpp
+++ CONTAINERS/inc/vector.hpp
@@ -6,16 +6,18 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:50:01 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/14 20:17:47 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/15 17:29:58 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
+# include <memory>
+
 namespace ft {
 
-template <typename T, typename Alloc>
+template <typename T, typename Alloc = std::allocator<T>>
 class vector {
 	
 };
