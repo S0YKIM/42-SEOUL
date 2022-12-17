907fd7b9ef033a6a523e221f13f6d55508237229
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-17 16:20:40 +0900

    feat: Implement member functions and non-member functions of stack class
    - Implement top(), empty(), size(), push(), pop() (member functions)
    - Implement relation operators such as ==, !=, <, <=, >, >= (non-member
      functions)

--- CONTAINERS/inc/stack.hpp
+++ CONTAINERS/inc/stack.hpp
@@ -6,21 +6,25 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:49:23 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/17 14:09:19 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/17 16:01:57 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP
 # define STACK_HPP
 
+# include <vector>
 # include <vector.hpp>
 # include <stack>
 
 namespace ft {
-template <typename T, typename Container = ft::vector<T>>
+template <typename T, typename Container = std::vector<T> >
 class stack {
+	friend bool operator==(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+	friend bool operator< (const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+
 public:
-	typedef typename Container					container_type;
+	typedef Container							container_type;
 	typedef typename Container::value_type		value_type;
 	typedef typename Container::size_type		size_type;
 	typedef typename Container::reference		reference;
@@ -31,14 +35,44 @@ protected:
 
 public:
 	/* Default constructor or copy constructor */
-	explicit stack(const Container& c = Container()) : c_(c) {};
+	explicit stack(const container_type& c = container_type()) : c_(c) {};
 
 	/* Returns a read/write reference to the data at the first element of the stack */
-	reference	top() { return c.back(); };
+	reference top() { return c_.back(); };
 
 	/* Returns a read-only constant reference to the data at the first element of the stack */
-	const_reference	top() const { return c.back(); };
+	const_reference top() const { return c_.back(); };
+
+	/* Returns true if the stack is empty */
+	bool empty() const { return c_.empty(); };
+
+	/* Returns the number of elements in the stack */
+	size_type size() const { return c_.size(); };
+
+	/* Creates an element at the top of the stack and assigns the given data to it */
+	void push(const value_type& value) { c_.push_back(value); };
+
+	/* Removes the top element from the stack */
+	void pop() { c_.pop_back(); };
 };
-}
+
+template <typename T, typename Container>
+inline bool operator==(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return lhs.c_ == rhs.c_; };
+
+template <typename T, typename Container>
+inline bool operator<(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return lhs.c_ < rhs.c_; };
+
+template <typename T, typename Container>
+inline bool operator!=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(lhs == rhs); };
+
+template <typename T, typename Container>
+inline bool operator<=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(rhs < lhs); };
+
+template <typename T, typename Container>
+inline bool operator>(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return rhs < lhs; };
+
+template <typename T, typename Container>
+inline bool operator>=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(lhs < rhs); };
+} // namespace ft
 
 #endif
