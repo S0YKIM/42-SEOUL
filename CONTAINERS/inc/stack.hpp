0bb496fe9eb856c27b108168c2c8c24ddff1832a
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-17 20:11:36 +0900

    fix: Fix compile error
    - Add template <typename T1, typename C1> in front of declaration of
      friend function
    - Recover inline keyword in operators as it turned out to follow c++98
      standard

--- CONTAINERS/inc/stack.hpp
+++ CONTAINERS/inc/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:49:23 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/17 16:01:57 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/17 19:57:51 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,23 @@
 
 namespace ft {
 template <typename T, typename Container = std::vector<T> >
-class stack {
-	friend bool operator==(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
-	friend bool operator< (const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+class stack;
 
+template <typename T, typename Container>
+inline bool operator==(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+
+template <typename T, typename Container>
+inline bool operator<(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+
+template <typename T, typename Container>
+class stack {
 public:
+	template <typename T1, typename C1>
+	friend bool operator==(const ft::stack<T1, C1>& lhs, const ft::stack<T1, C1>& rhs);
+
+	template <typename T1, typename C1>
+	friend bool operator< (const ft::stack<T1, C1>& lhs, const ft::stack<T1, C1>& rhs);
+
 	typedef Container							container_type;
 	typedef typename Container::value_type		value_type;
 	typedef typename Container::size_type		size_type;
@@ -35,44 +47,44 @@ protected:
 
 public:
 	/* Default constructor or copy constructor */
-	explicit stack(const container_type& c = container_type()) : c_(c) {};
+	explicit stack(const container_type& c = container_type()) : c_(c) {}
 
 	/* Returns a read/write reference to the data at the first element of the stack */
-	reference top() { return c_.back(); };
+	reference top() { return c_.back(); }
 
 	/* Returns a read-only constant reference to the data at the first element of the stack */
-	const_reference top() const { return c_.back(); };
+	const_reference top() const { return c_.back(); }
 
 	/* Returns true if the stack is empty */
-	bool empty() const { return c_.empty(); };
+	bool empty() const { return c_.empty(); }
 
 	/* Returns the number of elements in the stack */
-	size_type size() const { return c_.size(); };
+	size_type size() const { return c_.size(); }
 
 	/* Creates an element at the top of the stack and assigns the given data to it */
-	void push(const value_type& value) { c_.push_back(value); };
+	void push(const value_type& value) { c_.push_back(value); }
 
 	/* Removes the top element from the stack */
-	void pop() { c_.pop_back(); };
+	void pop() { c_.pop_back(); }
 };
 
 template <typename T, typename Container>
-inline bool operator==(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return lhs.c_ == rhs.c_; };
+inline bool operator==(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return lhs.c_ == rhs.c_; }
 
 template <typename T, typename Container>
-inline bool operator<(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return lhs.c_ < rhs.c_; };
+inline bool operator<(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return lhs.c_ < rhs.c_; }
 
 template <typename T, typename Container>
-inline bool operator!=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(lhs == rhs); };
+inline bool operator!=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(lhs == rhs); }
 
 template <typename T, typename Container>
-inline bool operator<=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(rhs < lhs); };
+inline bool operator<=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(rhs < lhs); }
 
 template <typename T, typename Container>
-inline bool operator>(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return rhs < lhs; };
+inline bool operator>(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return rhs < lhs; }
 
 template <typename T, typename Container>
-inline bool operator>=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(lhs < rhs); };
+inline bool operator>=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs) { return !(lhs < rhs); }
 } // namespace ft
 
 #endif
