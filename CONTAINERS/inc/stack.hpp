aae4ae92da8f77153e240d405df7065f90a859dd
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-17 13:49:31 +0900

    feat: Add member types and member functions of stack class

--- CONTAINERS/inc/stack.hpp
+++ CONTAINERS/inc/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:49:23 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/15 17:28:41 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/17 13:48:00 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,30 @@
 # define STACK_HPP
 
 # include <vector.hpp>
+# include <stack>
 
 namespace ft {
 template <typename T, typename Container = ft::vector<T>>
 class stack {
+public:
+	typedef typename Container					container_type;
+	typedef typename Container::value_type		value_type;
+	typedef typename Container::size_type		size_type;
+	typedef typename Container::reference		reference;
+	typedef typename Container::const_reference	const_reference;
 
+public:
+	/* Default constructor or copy constructor */
+	explicit stack(const Container& cont = Container()) {};
+
+	/* Copy constructor */
+	stack(const stack& other) {};
+
+	/* Copy assignment operator overloading */
+	stack& operator=(const stack& other) {};
+
+	/* Destructor */
+	~stack() {};
 };
 }
 
