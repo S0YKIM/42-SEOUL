4a5ece0f1902d56e027b8557b2c18af1a0ee1873
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-22 15:55:52 +0900

    feat: Add template class vector

--- CONTAINERS/inc/vector.hpp
+++ CONTAINERS/inc/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:50:01 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/17 16:22:36 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/19 18:22:58 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 # include <memory>
 
 namespace ft {
-
-template <typename T, typename Alloc = std::allocator<T> >
+template <typename T, typename Allocator = std::allocator<T> >
 class vector {
 	
 };
