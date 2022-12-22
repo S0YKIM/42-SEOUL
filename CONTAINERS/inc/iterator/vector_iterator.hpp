3e083baac7872cb16c2b12e603cbe589d2c6b904
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-22 21:31:14 +0900

    add: Add vector iterator class

---
+++ CONTAINERS/inc/iterator/vector_iterator.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_iterator.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/22 21:11:36 by sokim             #+#    #+#             */
+/*   Updated: 2022/12/22 21:13:49 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_ITERATOR_HPP
+# define VECTOR_ITERATOR_HPP
+
+namespace ft {
+template <typename Iter>
+class vector_iterator : public iterator {
+	
+};
+}
+
+#endif
