85c8291efe410beefa227a3453830fd17c44c6e1
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-27 12:54:13 +0900

    feat: Implement relational operators for vector

--- CONTAINERS/inc/iterator/vector_iterator.hpp
+++ CONTAINERS/inc/iterator/vector_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 21:11:36 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/26 14:48:30 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/27 12:41:55 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,55 +44,133 @@ public:
 	/* Getter function for current */
 	const Iter &base() const { return current_; }
 
-	/* Forward iterator requirements */
+	/* Operator overloading */
+	// SECTION Forward iterator requirements
 	reference operator*() const { return *current_; }
 	pointer operator->() const { return current_; }
 	vector_iterator &operator++() {
 		++current_;
 		return *this;
 	}
+
 	vector_iterator operator++(int) {
 		vector_iterator	tmp(current_);
 
 		++current_;
 		return tmp;
-	}
+	} // !SECTION
 
-	/* Bidirectional iterator requirements */
+	// SECTION Bidirectional iterator requirements
 	vector_iterator &operator--() {
 		--current_;
 		return *this;
 	}
+
 	vector_iterator oprator--(int) {
 		vector_iterator	tmp(current_);
 
 		--current_;
 		return tmp;
-	}
+	} // !SECTION
 
-	/* Random access iterator requirements */
+	// SECTION Random access iterator requirements
 	reference operator[](const difference_type& n) const { return current_[n]; }
 	vector_iterator operator+(const difference_type &n) const {
 		vector_iterator tmp(current_ + n);
 
 		return tmp;
 	}
+
 	vector_iterator &operator+=(const difference_type &n) {
 		current_ += n;
 		return *this;
 	}
+
 	vector_iterator operator-(const difference_type &n) const {
 		vector_iterator tmp(current_ - n);
 
 		return tmp;
 	}
+
 	vector_iterator &operator-=(const difference_type &n) {
 		current_ -= n;
 		return *this;
-	}
+	} // !SECTION
 };
 
 /* Relational operators */
+// SECTION Forward iterator requirements
+template<typename IterTypeL, typename IterTypeR>
+inline bool operator==(const vector_iterator<IterTypeL> &lhs, const vector_iterator<IterTypeR> &rhs) {
+	return lhs.base() == rhs.base();
+}
 
+template<typename IterTypeL, typename IterTypeR>
+inline bool operator!=(const vector_iterator<IterTypeL> &lhs, const vector_iterator<IterTypeR> &rhs) {
+	return lhs.base() != rhs.base();
+}
+
+template<typename IterType>
+inline bool operator==(const vector_iterator<IterType> &lhs, const vector_iterator<IterType> &rhs) {
+	return lhs.base() == rhs.base();
+}
+
+template<typename IterType>
+inline bool operator!=(const vector_iterator<IterType> &lhs, const vector_iterator<IterType> &rhs) {
+	return lhs.base() != rhs.base();
+} // !SECTION
+
+// SECTION Random access iterator requirements
+template<typename IterTypeL, typename IterTypeR>
+inline bool operator<(const vector_iterator<IterTypeL> &lhs, const vector_iterator<IterTypeR> &rhs) {
+	return lhs.base() < rhs.base();
+}
+
+template<typename IterTypeL, typename IterTypeR>
+inline bool operator<=(const vector_iterator<IterTypeL> &lhs, const vector_iterator<IterTypeR> &rhs) {
+	return lhs.base() <= rhs.base();
+}
+
+template<typename IterTypeL, typename IterTypeR>
+inline bool operator>(const vector_iterator<IterTypeL> &lhs, const vector_iterator<IterTypeR> &rhs) {
+	return lhs.base() > rhs.base();
+}
+
+template<typename IterTypeL, typename IterTypeR>
+inline bool operator>=(const vector_iterator<IterTypeL> &lhs, const vector_iterator<IterTypeR> &rhs) {
+	return lhs.base() >= rhs.base();
+}
+
+template<typename IterType>
+inline bool operator<(const vector_iterator<IterType> &lhs, const vector_iterator<IterType> &rhs) {
+	return lhs.base() < rhs.base();
+}
+
+template<typename IterType>
+inline bool operator<=(const vector_iterator<IterType> &lhs, const vector_iterator<IterType> &rhs) {
+	return lhs.base() <= rhs.base();
+}
+
+template<typename IterType>
+inline bool operator>(const vector_iterator<IterType> &lhs, const vector_iterator<IterType> &rhs) {
+	return lhs.base() > rhs.base();
+}
+
+template<typename IterType>
+inline bool operator>=(const vector_iterator<IterType> &lhs, const vector_iterator<IterType> &rhs) {
+	return lhs.base() >= rhs.base();
+} // !SECTION
+
+template<typename IterTypeL, typename IterTypeR>
+inline typename vector_iterator<IterTypeL>::difference_type operator-(const vector_iterator<IterTypeL> &lhs, const vector_iterator<IterTypeR> &rhs) {
+	return lhs.base() - rhs.base();
+}
+
+template<typename IterType>
+inline vector_iterator<IterType> operator+(const typename vector_iterator<IterType>::difference_type n, const vector_iterator<IterType> &i) {
+	vector_iterator	tmp(n + i.base());
+
+	return tmp;
+}
 }
 #endif
