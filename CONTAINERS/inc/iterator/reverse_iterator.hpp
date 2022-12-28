6a47e740ecf9a03c739c4c15177658de8dbd3473
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-28 19:04:52 +0900

    feat: Implement reverse_iterator class
    - Inherit from iterator class
    - Add typedef of some iterator traits which are used in this class
    - Implement constructors, getter function, and operators

--- CONTAINERS/inc/iterator/reverse_iterator.hpp
+++ CONTAINERS/inc/iterator/reverse_iterator.hpp
@@ -6,18 +6,137 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 13:50:36 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/22 21:15:24 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/28 19:02:50 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REVERSE_ITERATOR_HPP
-# define REVERSE_ITERATOR_HPP
+#define REVERSE_ITERATOR_HPP
 
-# include "iterator_base_types.hpp"
+#include <type_traits>
+
+#include "iterator_base_types.hpp"
 
 namespace ft {
+template <typename Iter>
+class reverse_iterator
+    : public iterator<typename iterator_traits<Iter>::iterator_category,
+                      typename iterator_traits<Iter>::value_type,
+                      typename iterator_traits<Iter>::difference_type,
+                      typename iterator_traits<Iter>::pointer,
+                      typename iterator_traits<Iter>::reference> {
+ protected:
+  Iter current;
 
+ public:
+  typedef typename iterator_traits<Iter> traits;
 
+  typedef typename traits::difference_type difference_type;
+  typedef typename traits::reference reference;
+  typedef typename traits::pointer pointer;
+
+ public:
+  /* Default constructor */
+  reverse_iterator() : current() {}
+
+  /* Constructor to set current using base iterator */
+  explicit reverse_iterator(Iter baseIt) : current(baseIt) {}
+
+  /* Normal copy constructor */
+  reverse_iterator(const reverse_iterator &other) : current(other.current) {}
+
+  /* Copy constructor copying from other type */
+  template <typename IterType>
+  reverse_iterator(const reverse_iterator<IterType> &other)
+      : current(other.base()) {}
+
+  /* Getter function for current */
+  Iter base() const { return current; }
+
+  // NOTE: Operator overloading
+  // SECTION: Dereference operators
+  /* Return element of which position is just before current */
+  reference operator*() const {
+    Iter tmp = current;
+
+    tmp--;
+    return *tmp;
+  }
+
+  pointer operator->() const {
+    Iter tmp = current;
+
+    tmp--;
+    return tmp
+  }  // !SECTION
+
+  // SECTION: Subscript operator
+  reference operator[](difference_type n) const {
+    Iter tmp = current - n;
+
+    return *tmp;
+  }  // !SECTION
+
+  // SECTION: Increment and decrement operators
+  reverse_iterator &operator++() {
+    current--;
+    return *this;
+  }
+
+  reverse_iterator operator++(int) {
+    reverse_iterator tmp(*this);
+    current--;
+    return tmp;
+  }
+
+  reverse_iterator &operator--() {
+    current++;
+    return *this;
+  }
+
+  reverse_iterator operator--(int) {
+    reverse_iterator tmp(*this);
+    current++;
+    return tmp;
+  }  // !SECTION
+
+  // SECTION: Arithmetic operators
+  reverse_iterator operator+(difference_type n) const {
+    reverse_iterator tmp(current - n);
+
+    return tmp;
+  }
+
+  reverse_iterator &operator+=(difference_type n) {
+    current -= n;
+    return *this;
+  }
+
+  reverse_iterator operator-(difference_type n) const {
+    reverse_iterator tmp(current + n);
+
+    return tmp;
+  }
+
+  reverse_iterator &operator-=(difference_type n) {
+    current += n;
+    return *this;
+  }  // !SECTION
+};
+
+// NOTE: Non-member functions
+// SECTION: Relational operators
+template <typename Iter>
+inline bool operator==(const reverse_iterator<Iter> &lhs,
+                       const reverse_iterator<Iter> &rhs) {
+  return lhs.base() == rhs.base();
 }
 
+template <typename Iter>
+inline bool operator<(const reverse_iterator<Iter> &lhs,
+                      const reverse_iterator<Iter> &rhs) {
+  return lhs.base() > rhs.base();
+}
+// !SECTION
+}  // namespace ft
 #endif
