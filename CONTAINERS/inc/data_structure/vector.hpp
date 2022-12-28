7e134c2d28b5e33204e6c199da5dfa61a47a6b95
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-28 16:04:34 +0900

    style: Apply Google C++ style format

--- CONTAINERS/inc/data_structure/vector.hpp
+++ CONTAINERS/inc/data_structure/vector.hpp
@@ -6,20 +6,18 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:50:01 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/19 18:22:58 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/28 16:04:18 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
-# define VECTOR_HPP
+#define VECTOR_HPP
 
-# include <memory>
+#include <memory>
 
 namespace ft {
 template <typename T, typename Allocator = std::allocator<T> >
-class vector {
-	
-};
-}
+class vector {};
+}  // namespace ft
 
 #endif
