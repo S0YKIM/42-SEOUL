c392ef5883401c3fda27ba1c0a1e1d6ec1a44926
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-28 16:03:39 +0900

    style: Apply Google C++ style format

--- CONTAINERS/inc/data_structure/map.hpp
+++ CONTAINERS/inc/data_structure/map.hpp
@@ -6,11 +6,11 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:49:41 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/14 19:49:57 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/28 16:03:08 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_HPP
-# define MAP_HPP
+#define MAP_HPP
 
 #endif
