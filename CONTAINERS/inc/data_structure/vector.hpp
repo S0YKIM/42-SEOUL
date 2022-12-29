47cf4e384554a7c71b33a1f06967e8ab762e1ba0
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-12-29 17:29:38 +0900

    feat: Implement member types

--- CONTAINERS/inc/data_structure/vector.hpp
+++ CONTAINERS/inc/data_structure/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:50:01 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/28 16:04:18 by sokim            ###   ########.fr       */
+/*   Updated: 2022/12/29 17:29:03 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,27 @@
 
 #include <memory>
 
+#include "reverse_iterator.hpp"
+#include "vector_iterator.hpp"
+
 namespace ft {
 template <typename T, typename Allocator = std::allocator<T> >
-class vector {};
+class vector {
+ public:
+  typedef Allocator<T> allocator_type;
+
+  typedef typename allocator_type::reference reference;
+  typedef typename allocator_type::const_reference const_reference;
+  typedef typename allocator_type::pointer pointer;
+  typedef typename allocator_type::const_pointer const_pointer;
+  typedef vector_iterator<pointer> iterator;
+  typedef vector_iterator<const_pointer> const_iterator;
+  typedef reverse_iterator<iterator> reverse_iterator;
+  typedef reverse_iterator<const_iterator> const_reverse_iterator;
+
+  typedef ptrdiff_t difference_type;
+  typedef size_t size_type;
+};
 }  // namespace ft
 
 #endif
