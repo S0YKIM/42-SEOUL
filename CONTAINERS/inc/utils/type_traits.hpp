e44e157fbf4184a1ad27ec57bf03e78d2edcd2e7
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-05 11:53:08 +0900

    feat: Implement is_integral class template

--- CONTAINERS/inc/utils/type_traits.hpp
+++ CONTAINERS/inc/utils/type_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 10:55:02 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/04 15:27:17 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/05 11:52:31 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,85 @@ typedef integral_constant<bool, false> false_type;
  * @tparam T The type to be checked whether it is integral or not
  */
 template <typename T>
-struct is_integral : public false_type {};
+struct _is_integral : public false_type {};
+
+/**
+ * @brief Template specialization for every integral type
+ *
+ * If is_integral<X>::value is true, than X is integral.
+ */
+template <>
+struct _is_integral<bool> : public true_type {};
+
+template <>
+struct _is_integral<char> : public true_type {};
+
+template <>
+struct _is_integral<wchar_t> : public true_type {};
+
+template <>
+struct _is_integral<signed char> : public true_type {};
+
+template <>
+struct _is_integral<short int> : public true_type {};
+
+template <>
+struct _is_integral<int> : public true_type {};
+
+template <>
+struct _is_integral<long int> : public true_type {};
+
+template <>
+struct _is_integral<long long int> : public true_type {};
+
+template <>
+struct _is_integral<unsigned char> : public true_type {};
+
+template <>
+struct _is_integral<unsigned short int> : public true_type {};
+
+template <>
+struct _is_integral<unsigned int> : public true_type {};
+
+template <>
+struct _is_integral<unsigned long int> : public true_type {};
+
+template <>
+struct _is_integral<unsigned long long int> : public true_type {};
+
+/**
+ * @brief Remove const and volatile keyword from the given type
+ *
+ * Since is_integral needs to consider cv-qualified integral types as integral,
+ * it provides typedef for the pure type without const and volatile keyword.
+ */
+template <typename T>
+struct _remove_const_volatile {
+  typedef T type;
+};
+
+template <typename T>
+struct _remove_const_volatile<const T> {
+  typedef T type;
+};
+
+template <typename T>
+struct _remove_const_volatile<volatile T> {
+  typedef T type;
+};
+
+template <typename T>
+struct _remove_const_volatile<const volatile T> {
+  typedef T type;
+};
+
+/**
+ * @brief Tell if the given type is integral or not
+ *
+ * Inherits _is_integral struct to remove const and volatile keyword.
+ */
+template <typename T>
+struct is_integral
+    : public _is_integral<typename _remove_const_volatile<T>::type> {};
 
 #endif
