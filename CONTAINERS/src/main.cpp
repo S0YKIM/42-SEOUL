a8ec7fa451aaf8d321455c922861e6956cee05b7
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-12 20:30:17 +0900

    test: Add main.cpp for test

--- CONTAINERS/src/main.cpp
+++ CONTAINERS/src/main.cpp
@@ -5,120 +5,42 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/14 19:30:21 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/11 17:32:14 by sokim            ###   ########.fr       */
+/*   Created: 2023/01/12 20:07:15 by sokim             #+#    #+#             */
+/*   Updated: 2023/01/12 20:29:12 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <deque>
 #include <iostream>
-#include <string>
 
-#if 1  // CREATE A REAL STL EXAMPLE
-#include <map>
-#include <stack>
-#include <vector>
-namespace ft = std;
-#else
-#include <map.hpp>
-#include <stack.hpp>
-#include <vector.hpp>
-#endif
+int main() {
+  int option = 0;
 
-#include <stdlib.h>
-#include <time.h>  // time()
+  std::cout << "****************************************" << std::endl;
+  std::cout << "*          Enter number to test        *" << std::endl;
+  std::cout << "****************************************" << std::endl;
+  std::cout << "* [1] - Vector                         *" << std::endl;
+  std::cout << "* [2] - Stack                          *" << std::endl;
+  std::cout << "* [3] - Map                            *" << std::endl;
+  std::cout << "* [4] - Set                            *" << std::endl;
+  std::cout << "****************************************" << std::endl;
+  std::cin >> option;
 
-#define MAX_RAM 4294967296
-#define BUFFER_SIZE 4096
-struct Buffer {
-  int idx;
-  char buff[BUFFER_SIZE];
-};
-
-#define COUNT (MAX_RAM / (int)sizeof(Buffer))
-
-template <typename T>
-class MutantStack : public ft::stack<T> {
- public:
-  MutantStack() {}
-  MutantStack(const MutantStack<T>& src) { *this = src; }
-  MutantStack<T>& operator=(const MutantStack<T>& rhs) {
-    this->c = rhs.c;
-    return *this;
+  switch (option) {
+    case 1:
+      std::cout << "1" << std::endl;
+      break;
+    case 2:
+      std::cout << "2" << std::endl;
+      break;
+    case 3:
+      std::cout << "3" << std::endl;
+      break;
+    case 4:
+      std::cout << "4" << std::endl;
+      break;
+    default:
+      std::cout << "Please enter a valid number." << std::endl;
+      break;
   }
-  ~MutantStack() {}
-
-  typedef typename ft::stack<T>::container_type::iterator iterator;
-
-  iterator begin() { return this->c.begin(); }
-  iterator end() { return this->c.end(); }
-};
-
-int main(int argc, char** argv) {
-  time_t start, end;
-  time_t result;
-
-  start = time(NULL);
-  if (argc != 2) {
-    std::cerr << "Usage: ./test seed" << std::endl;
-    std::cerr << "Provide a seed please" << std::endl;
-    std::cerr << "Count value:" << COUNT << std::endl;
-    return 1;
-  }
-  const int seed = atoi(argv[1]);
-  srand(seed);
-
-  ft::vector<std::string> vector_str;
-  ft::vector<int> vector_int;
-  ft::stack<int> stack_int;
-  ft::vector<Buffer> vector_buffer;
-  ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
-  //   ft::map<int, int> map_int;
-
-  for (int i = 0; i < COUNT; i++) {
-    vector_buffer.push_back(Buffer());
-  }
-
-  for (int i = 0; i < COUNT; i++) {
-    const int idx = rand() % COUNT;
-    vector_buffer[idx].idx = 5;
-  }
-  ft::vector<Buffer>().swap(vector_buffer);
-
-  try {
-    for (int i = 0; i < COUNT; i++) {
-      const int idx = rand() % COUNT;
-      vector_buffer.at(idx);
-      std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" << std::endl;
-    }
-  } catch (const std::exception& e) {
-    // NORMAL ! :P
-  }
-
-  //   for (int i = 0; i < COUNT; ++i) {
-  //     map_int.insert(ft::make_pair(rand(), rand()));
-  //   }
-
-  //   int sum = 0;
-  //   for (int i = 0; i < 10000; i++) {
-  //     int access = rand();
-  //     sum += map_int[access];
-  //   }
-  //   std::cout << "should be constant with the same seed: " << sum <<
-  //   std::endl;
-
-  //   { ft::map<int, int> copy = map_int; }
-  //   MutantStack<char> iterable_stack;
-  //   for (char letter = 'a'; letter <= 'z'; letter++)
-  //   iterable_stack.push(letter); for (MutantStack<char>::iterator it =
-  //   iterable_stack.begin();
-  //        it != iterable_stack.end(); it++) {
-  //     std::cout << *it;
-  //   }
-  //   std::cout << std::endl;
-  end = time(NULL);
-  result = end - start;
-  std::cout << "Execution time: " << result << " seconds"
-            << "\n";
-  return (0);
+  return 0;
 }
