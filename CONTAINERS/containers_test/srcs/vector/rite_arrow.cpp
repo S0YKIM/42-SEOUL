568a7e3883bcebe32894da614cadbee4b3e3de04
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-12 19:40:55 +0900

    fix: Fix tester since c++98 does not support comparing iterators with different types

--- CONTAINERS/containers_test/srcs/vector/rite_arrow.cpp
+++ CONTAINERS/containers_test/srcs/vector/rite_arrow.cpp
@@ -2,45 +2,43 @@
 
 #define TESTED_TYPE foo<int>
 
-int		main(void)
-{
-	const int size = 5;
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it(vct.rbegin());
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator ite(vct.rend());
+int main(void) {
+  const int size = 5;
+  TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
+  TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it(vct.rbegin());
+  TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator ite(vct.rend());
 
-	for (int i = 1; it != ite; ++i)
-		*it++ = (i * 7);
-	printSize(vct, 1);
+  for (int i = 1; it != ite; ++i) *it++ = (i * 7);
+  printSize(vct, 1);
 
-	it = vct.rbegin();
-	ite = vct.rbegin();
+  it = vct.rbegin();
+  ite = vct.rbegin();
 
-	std::cout << *(++ite) << std::endl;
-	std::cout << *(ite++) << std::endl;
-	std::cout << *ite++ << std::endl;
-	std::cout << *++ite << std::endl;
+  std::cout << *(++ite) << std::endl;
+  std::cout << *(ite++) << std::endl;
+  std::cout << *ite++ << std::endl;
+  std::cout << *++ite << std::endl;
 
-	it->m();
-	ite->m();
+  it->m();
+  ite->m();
 
-	std::cout << *(++it) << std::endl;
-	std::cout << *(it++) << std::endl;
-	std::cout << *it++ << std::endl;
-	std::cout << *++it << std::endl;
+  std::cout << *(++it) << std::endl;
+  std::cout << *(it++) << std::endl;
+  std::cout << *it++ << std::endl;
+  std::cout << *++it << std::endl;
 
-	std::cout << *(--ite) << std::endl;
-	std::cout << *(ite--) << std::endl;
-	std::cout << *--ite << std::endl;
-	std::cout << *ite-- << std::endl;
+  std::cout << *(--ite) << std::endl;
+  std::cout << *(ite--) << std::endl;
+  std::cout << *--ite << std::endl;
+  std::cout << *ite-- << std::endl;
 
-	(*it).m();
-	(*ite).m();
+  (*it).m();
+  (*ite).m();
 
-	std::cout << *(--it) << std::endl;
-	std::cout << *(it--) << std::endl;
-	std::cout << *it-- << std::endl;
-	std::cout << *--it << std::endl;
+  std::cout << *(--it) << std::endl;
+  std::cout << *(it--) << std::endl;
+  std::cout << *it-- << std::endl;
+  std::cout << *--it << std::endl;
 
-	return (0);
+  return (0);
 }
