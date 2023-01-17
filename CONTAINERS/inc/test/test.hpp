d7a30f8e0543493f916fc71d08ffd80c69ff77d3
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-17 15:26:20 +0900

    modify: Remove include <vector.hpp>

--- CONTAINERS/inc/test/test.hpp
+++ CONTAINERS/inc/test/test.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:39:06 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/17 14:15:31 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/17 15:20:30 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #define TEST_HPP
 
 #include <iostream>
-
-#include "vector.hpp"
 
 void test_vector();
 void test_const_vector();
