b71a64e7917ec39a3e2e1176038957ebbbcfaf00
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-20 15:35:56 +0900

    feat: Implement _rb_tree_iterator_base

---
+++ CONTAINERS/inc/iterator/rb_tree_iterator.hpp
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rb_tree_iterator.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 15:33:29 by sokim             #+#    #+#             */
+/*   Updated: 2023/01/20 15:33:53 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RB_TREE_ITERATOR_HPP
+#define RB_TREE_ITERATOR_HPP
+
+#include <cstddef>  // std::ptrdiff_t
+#include <iterator>
+
+namespace ft {
+enum _rb_tree_color { RED, BLACK };
+
+/**
+ * @brief Base class of _rb_tree_node class
+ *
+ * Because it will be only used to insert, delete, and rebalance in rbtree, it
+ * doesn't need the info of the value that each node has.
+ */
+struct _rb_tree_node_base {
+  typedef _rb_tree_node_base* _base_ptr;
+
+  _rb_tree_color _color;
+
+  _base_ptr _parent;
+  _base_ptr _left_child;
+  _base_ptr _right_child;
+
+  static _base_ptr minimum(_base_ptr node) {
+    while (node->_left_child) node = node->_left_child;
+    return node;
+  }
+
+  static _base_ptr maximum(_base_ptr node) {
+    while (node->_right_child) node = node->_right_child;
+    return node;
+  }
+};
+
+template <typename value_type>
+struct _rb_tree_node : public rb_tree_node_base {
+  typedef _rb_tree_node<value_type>* _link_type;
+  value_type _value;
+};
+
+struct _rb_tree_base_iterator {
+  typedef _rb_tree_node_base::_base_ptr _base_ptr;
+  typedef std::bidirectional_iterator_tag iterator_category;
+  typedef std::ptrdiff_t difference_type;
+
+  _base_ptr _node;
+
+  /**
+   * @brief Increment the base iterator.
+   *
+   * If you increment the rightmost node, then it will become the header node
+   * which represents nil node.
+   */
+  void _increment() {
+    if (_node->_right_child) {
+      _node = _node->_right_child;
+      while (_node->_left_child) _node = _node->_left_child;
+    } else {
+      _base_ptr tmp = _node->_parent;
+      while (_node == tmp->_right_child) {
+        _node = tmp;
+        tmp = tmp->_parent;
+      }
+      if (_node->_right_child != tmp) _node = tmp;
+    }
+  }
+
+  /**
+   * @brief Decrement the base iterator.
+   *
+   * If you decrement the header node, then it will become the rightmost node.
+   */
+  void _decrement() {
+    if (_node->_color == RED && _node->_parent->_parent == _node)
+      _node = _node->_right_child;
+    else if (_node->_left_child) {
+      _base_ptr tmp = _node->_left_child;
+      while (tmp->_right_child) tmp = tmp->_right_child;
+      _node = tmp;
+    } else {
+      _base_ptr tmp = _node->_parent;
+      while (_node == tmp->_left_child) {
+        _node = tmp;
+        tmp = tmp->_parent;
+      }
+      _node = tmp;
+    }
+  }
+};
+}  // namespace ft
+
+#endif
