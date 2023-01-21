c01ba7dc174c7bf71357d0ebfeccac8ffbc51be9
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-21 21:05:44 +0900

    add: Add _rb_tree class

--- CONTAINERS/inc/data_structure/_rb_tree.hpp
+++ CONTAINERS/inc/data_structure/_rb_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 17:10:51 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/20 15:34:20 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/21 21:01:59 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 
 #include "rb_tree_iterator.hpp"
 
-namespace ft {}  // namespace ft
+namespace ft {
+// TODO: Implement _rb_tree
+template <typename Key, typename Val, typename KeyOfValue, typename Compare,
+          typename Alloc = std::allocator<Val> >
+class _rb_tree {};
+}  // namespace ft
 
 #endif
