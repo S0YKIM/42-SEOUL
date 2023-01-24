29d12df62ace7fbb9cb6209d4c1e0fe7eeecceaa
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-24 16:35:41 +0900

    refactor: Move implementation of functions

--- CONTAINERS/inc/data_structure/_rb_tree.hpp
+++ CONTAINERS/inc/data_structure/_rb_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 17:10:51 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/24 16:12:56 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/24 16:33:25 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,53 +16,12 @@
 #include "rb_tree_iterator.hpp"
 
 namespace ft {
-/**
- * @brief Left rotate
- *
- * @param node The node to be pulled down
- * @param root The root node of the tree
- */
 inline void _rb_tree_rotate_left(_rb_tree_node_base* node,
-                                 _rb_tree_node_base*& root) {
-  _rb_tree_node_base* tmp = node->_right_child;
-  node->_right_child = tmp->_left_child;
-  if (tmp->_left_child) tmp->_left_child->_parent = node;
-  tmp->_parent = node->_parent;
+                                 _rb_tree_node_base*& root);
 
-  if (node == root)
-    root = tmp;
-  else if (node == node->_parent->_left_child)
-    node->_parent->_left_child = tmp;
-  else
-    node->_parent->_right_child = tmp;
-
-  tmp->_left_child = node;
-  node->_parent = tmp;
-}
-
-/**
- * @brief Right rotate
- *
- * @param node The node to be pulled down
- * @param root The root node of the tree
- */
 inline void _rb_tree_rotate_right(_rb_tree_node_base* node,
-                                  _rb_tree_node_base*& root) {
-  _rb_tree_node_base* tmp = node->_left_child;
-  node->_left_child = tmp->_right_child;
-  if (tmp->_right_child) tmp->_right_child->_parent = node;
-  tmp->_parent = node->_parent;
+                                  _rb_tree_node_base*& root);
 
-  if (node == root)
-    root = tmp;
-  else if (node == node->_parent->_right_child)
-    node->_parent->_right_child = tmp;
-  else
-    node->_parent->_left_child = tmp;
-
-  tmp->_right_child = node;
-  node->_parent = tmp;
-}
 // TODO: Implement _rb_tree
 template <typename Key, typename Val, typename KeyOfValue, typename Compare,
           typename Alloc = std::allocator<Val> >
---
+++ CONTAINERS/src/rbtree/_rb_tree.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   _rb_tree.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 16:29:36 by sokim             #+#    #+#             */
+/*   Updated: 2023/01/24 16:33:10 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "_rb_tree.hpp"
+namespace ft {
+/**
+ * @brief Left rotation.
+ *
+ * @param node The node to be pulled down
+ * @param root The root node of the tree
+ */
+inline void _rb_tree_rotate_left(_rb_tree_node_base* node,
+                                 _rb_tree_node_base*& root) {
+  _rb_tree_node_base* tmp = node->_right_child;
+  node->_right_child = tmp->_left_child;
+  if (tmp->_left_child) tmp->_left_child->_parent = node;
+  tmp->_parent = node->_parent;
+
+  if (node == root)
+    root = tmp;
+  else if (node == node->_parent->_left_child)
+    node->_parent->_left_child = tmp;
+  else
+    node->_parent->_right_child = tmp;
+
+  tmp->_left_child = node;
+  node->_parent = tmp;
+}
+
+/**
+ * @brief Right rotation.
+ *
+ * @param node The node to be pulled down
+ * @param root The root node of the tree
+ */
+inline void _rb_tree_rotate_right(_rb_tree_node_base* node,
+                                  _rb_tree_node_base*& root) {
+  _rb_tree_node_base* tmp = node->_left_child;
+  node->_left_child = tmp->_right_child;
+  if (tmp->_right_child) tmp->_right_child->_parent = node;
+  tmp->_parent = node->_parent;
+
+  if (node == root)
+    root = tmp;
+  else if (node == node->_parent->_right_child)
+    node->_parent->_right_child = tmp;
+  else
+    node->_parent->_left_child = tmp;
+
+  tmp->_right_child = node;
+  node->_parent = tmp;
+}
+}  // namespace ft
