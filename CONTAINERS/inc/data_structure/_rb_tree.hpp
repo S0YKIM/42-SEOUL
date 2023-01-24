25465fc49e061c948a0933acd49fb655ad2646bd
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-24 16:13:42 +0900

    feat: Implement _rb_tree_rotate_right()

--- CONTAINERS/inc/data_structure/_rb_tree.hpp
+++ CONTAINERS/inc/data_structure/_rb_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 17:10:51 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/24 16:08:52 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/24 16:12:56 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,30 @@ inline void _rb_tree_rotate_left(_rb_tree_node_base* node,
     node->_parent->_right_child = tmp;
 
   tmp->_left_child = node;
+  node->_parent = tmp;
+}
+
+/**
+ * @brief Right rotate
+ *
+ * @param node The node to be pulled down
+ * @param root The root node of the tree
+ */
+inline void _rb_tree_rotate_right(_rb_tree_node_base* node,
+                                  _rb_tree_node_base*& root) {
+  _rb_tree_node_base* tmp = node->_left_child;
+  node->_left_child = tmp->_right_child;
+  if (tmp->_right_child) tmp->_right_child->_parent = node;
+  tmp->_parent = node->_parent;
+
+  if (node == root)
+    root = tmp;
+  else if (node == node->_parent->_right_child)
+    node->_parent->_right_child = tmp;
+  else
+    node->_parent->_left_child = tmp;
+
+  tmp->_right_child = node;
   node->_parent = tmp;
 }
 // TODO: Implement _rb_tree
