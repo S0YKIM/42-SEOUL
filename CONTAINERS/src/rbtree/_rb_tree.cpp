8cad2d1b8904ae3995f14d39459c166d41a08455
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-24 17:11:07 +0900

    feat: Implement _rb_tree_rebalance_after_insertion()

--- CONTAINERS/src/rbtree/_rb_tree.cpp
+++ CONTAINERS/src/rbtree/_rb_tree.cpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:29:36 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/24 16:33:10 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/24 17:10:18 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,60 @@ inline void _rb_tree_rotate_right(_rb_tree_node_base* node,
 
   tmp->_right_child = node;
   node->_parent = tmp;
+}
+
+/**
+ * @brief Check if the tree follows the rb tree rule after an insertion.
+ *
+ * @param node The node which was inserted
+ * @param root The root node
+ */
+void _rb_tree_rebalance_after_insertion(_rb_tree_node_base* node,
+                                        _rb_tree_node_base*& root) {
+  node->_color = RED;
+  while (node != root && node->_parent->_color == RED) {
+    if (node->_parent == node->_parent->_parent->_left_child) {
+      _rb_tree_node_base* tmp = node->_parent->_parent->_right_child;
+      if (tmp && tmp->_color == RED) {
+        node->_parent->_color = BLACK;
+        tmp->_color = BLACK;
+        node->_parent->_parent->_color = RED;
+        node = node->_parent->_parent;
+      }
+      // Case 1 or 2
+      else {
+        // Case 2: < shape
+        // Change into Case 1 by left rotation
+        if (node == node->_parent->_right_child) {
+          node = node->_parent;
+          _rb_tree_rotate_left(node, root);
+        }
+        // Case 1: / shape
+        node->_parent->_color = BLACK;
+        node->_parent->_parent->_color = RED;
+        _rb_tree_rotate_right(node->_parent->_parent, root);
+      }
+    } else {
+      _rb_tree_node_base* tmp = node->_parent->_parent->_left_child;
+      if (tmp && tmp->_color == RED) {
+        node->_parent->_color = BLACK;
+        tmp->_color = BLACK;
+        node->_parent->_parent->_color = RED;
+        node = node->_parent->_parent;
+      } else {
+        // Case 2: > shape
+        // Change into Case 1 by right rotation
+        if (node == node->_parent->_left_child) {
+          node = node->_parent;
+          _rb_tree_rotate_right(node, root);
+        }
+        // Case 1: \ shape
+        node->_parent->_color = BLACK;
+        node->_parent->_parent->_color = RED;
+        _rb_tree_rotate_left(node->_parent->_parent, root);
+      }
+    }
+  }
+  root->_color = BLACK;
 }
 }  // namespace ft
