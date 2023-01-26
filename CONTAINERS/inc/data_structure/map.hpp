6efef382f13be3c958f56a7dd9e0737e02b0a48b
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-26 20:40:04 +0900

    feat: Implement inner class value_compare

--- CONTAINERS/inc/data_structure/map.hpp
+++ CONTAINERS/inc/data_structure/map.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:49:41 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/26 19:53:32 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/26 20:36:28 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ namespace ft {
 template <class Key, class T, class Compare = std::less<Key>,
           class Allocator = std::allocator<pair<const Key, T>>>
 class map {
+ public:
   typedef Key key_type;
   typedef T mapped_type;
   typedef pair<const Key, T> value_type;
-
   typedef Compare key_compare;
   typedef Allocator allocator_type;
 
@@ -37,6 +37,31 @@ class map {
   typedef const value_type& const_reference;
   typedef typename Allocator::pointer pointer;
   typedef typename Allocator::const_pointer const_pointer;
+
+ private:
+  typedef _rb_tree<key_type, value_type, ft::select_first<value_type>,
+                   key_compare, allocator_type>
+      base_structure;
+  base_structure base;
+
+ public:
+  typedef typename base::iterator iterator;
+  typedef typename base::const_iterator const_iterator;
+  typedef typename base::reverse_iterator reverse_iterator;
+  typedef typename base::const_reverse_iterator const_reverse_iterator;
+
+  class value_compare {
+    friend class map<Key, T, Compare, Alloc>;
+
+   protected:
+    key_compare comp;
+    value_compare(key_compare c) : comp(c) {}
+
+   public:
+    bool operator()(const value_type& x, const value_type& y) const {
+      return comp(x.first, y.first);
+    }
+  };
 };
 }  // namespace ft
 
