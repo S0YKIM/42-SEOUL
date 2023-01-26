a2cdcbccade38a5859f4d4d092069c39d4859dbb
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-26 19:54:21 +0900

    feat: Add member types

--- CONTAINERS/inc/data_structure/map.hpp
+++ CONTAINERS/inc/data_structure/map.hpp
@@ -6,11 +6,38 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:49:41 by sokim             #+#    #+#             */
-/*   Updated: 2022/12/28 16:03:08 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/26 19:53:32 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_HPP
 #define MAP_HPP
+
+#include <cstddef>     // std::ptrdiff_t
+#include <functional>  // std::less
+#include <memory>      // std::allocator
+
+#include "_rb_tree.hpp"
+
+namespace ft {
+template <class Key, class T, class Compare = std::less<Key>,
+          class Allocator = std::allocator<pair<const Key, T>>>
+class map {
+  typedef Key key_type;
+  typedef T mapped_type;
+  typedef pair<const Key, T> value_type;
+
+  typedef Compare key_compare;
+  typedef Allocator allocator_type;
+
+  typedef size_t size_type;
+  typedef std::ptrdiff_t difference_type;
+
+  typedef value_type& reference;
+  typedef const value_type& const_reference;
+  typedef typename Allocator::pointer pointer;
+  typedef typename Allocator::const_pointer const_pointer;
+};
+}  // namespace ft
 
 #endif
