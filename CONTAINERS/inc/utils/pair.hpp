f157152162b7e54141d8fd76511fb4cf73c951af
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-01-26 19:30:09 +0900

    feat: Implement relational operators for pair

--- CONTAINERS/inc/utils/pair.hpp
+++ CONTAINERS/inc/utils/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:06:25 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/26 19:24:13 by sokim            ###   ########.fr       */
+/*   Updated: 2023/01/26 19:29:44 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ struct pair {
   pair() : first(), second() {}
 
   template <typename T, typename U>
-  pair(const pair<T, U>& other) : first(other.first), second(other.second) {}
+  pair(const pair<T, U> &other) : first(other.first), second(other.second) {}
 
-  pair(const first_type& a, const second_type& b) : first(a), second(b) {}
+  pair(const first_type &a, const second_type &b) : first(a), second(b) {}
 
-  pair& operator=(const pair& other) {
+  pair &operator=(const pair &other) {
     if (this != &other) {
       first = other.first;
       second = other.second;
@@ -37,11 +37,43 @@ struct pair {
     return *this;
   }
 
-  void swap(pair& other) {
+  void swap(pair &other) {
     std::swap(first, other.first);
     std::swap(second, other.second);
   }
 };
+
+template <typename T1, typename T2>
+bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+  return lhs.first == rhs.first && lhs.second == rhs.second;
+}
+
+template <typename T1, typename T2>
+bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+  return !(lhs == rhs);
+}
+
+template <typename T1, typename T2>
+bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+  return lhs.first < rhs.first ||
+         (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+}
+
+template <typename T1, typename T2>
+bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+  return rhs < lhs;
+}
+
+template <typename T1, typename T2>
+bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+  return !(lhs < rhs);
+}
+
+template <class _T1, class _T2>
+bool operator<=(const pair<_T1, _T2> &lhs, const pair<_T1, _T2> &rhs) {
+  return !(rhs < lhs);
+}
+
 }  // namespace ft
 
 #endif
