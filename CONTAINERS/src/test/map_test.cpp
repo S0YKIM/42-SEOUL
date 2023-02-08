38b795a2ff270e0c77374ece62252c5ea68d4446
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-02-08 19:18:24 +0900

    add: Add insert()

--- CONTAINERS/src/test/map_test.cpp
+++ CONTAINERS/src/test/map_test.cpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:32:00 by sokim             #+#    #+#             */
-/*   Updated: 2023/02/08 17:05:58 by sokim            ###   ########.fr       */
+/*   Updated: 2023/02/08 18:59:07 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void test_map() {
   std::cout << "*                 Insert               *" << std::endl;
   std::cout << "****************************************" << std::endl;
   std::cout << "- Insert 'a' to 'c'." << std::endl << std::endl;
-  // m.insert(ft::make_pair(1, "a"));
-  // m.insert(ft::make_pair(2, "b"));
-  // m.insert(ft::make_pair(3, "c"));
+  m.insert(ft::make_pair(1, "a"));
+  m.insert(ft::make_pair(2, "b"));
+  m.insert(ft::make_pair(3, "c"));
 }
