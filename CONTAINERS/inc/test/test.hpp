e96a523960626ba9142cb377df1e49f55909475e
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-02-08 18:07:34 +0900

    add: Add map test

--- CONTAINERS/inc/test/test.hpp
+++ CONTAINERS/inc/test/test.hpp
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:39:06 by sokim             #+#    #+#             */
-/*   Updated: 2023/01/17 15:20:30 by sokim            ###   ########.fr       */
+/*   Updated: 2023/02/08 16:06:50 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 
 void test_vector();
 void test_const_vector();
+
+void test_map();
 
 #endif
---
+++ CONTAINERS/src/test/map_test.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_test.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 20:32:00 by sokim             #+#    #+#             */
+/*   Updated: 2023/02/08 17:05:58 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "map.hpp"
+
+#include <sstream>
+#include <string>
+
+#include "test.hpp"
+
+void test_map() {
+  ft::map<int, std::string> m;
+
+  std::cout << "****************************************" << std::endl;
+  std::cout << "*                 Insert               *" << std::endl;
+  std::cout << "****************************************" << std::endl;
+  std::cout << "- Insert 'a' to 'c'." << std::endl << std::endl;
+  // m.insert(ft::make_pair(1, "a"));
+  // m.insert(ft::make_pair(2, "b"));
+  // m.insert(ft::make_pair(3, "c"));
+}
