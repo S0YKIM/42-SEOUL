2a0e3952bcb3b9ffeaa8cf75522c22e55ac2b9b8
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-02-23 21:39:16 +0900

    add: Add entrypoint.sh for mariadb

--- INCEPTION/srcs/requirements/mariadb/tools/entrypoint.sh
+++ INCEPTION/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -4,6 +4,18 @@
 set -e
 
 # Check whether system table is already setup or not
-if [ ! -d /var/lib/mysql/mysql ];
+if [ ! -d $DB_PATH/$DB_NAME ];
 then
-	mariadb-install-db --user=mysql
+	# Install mariadb according to my.cnf
+	mariadb-install-db
+
+	# Execute mariaDB server as background process
+	/usr/bin/mysqld_safe &
+
+	# Create database and user
+
+	# Stop mariadb server
+	mariadb-admin shutdown
+else
+	echo "Target database already exists.";
+fi
