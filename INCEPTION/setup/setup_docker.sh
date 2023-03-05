666184fe81e2b0ef687ce879b9d50db143337020
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-03-05 18:35:40 +0900

    feat: Create shell scripts for vm setup

---
+++ INCEPTION/setup/setup_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set up the repository
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker and Docker compose plugin
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
---
+++ INCEPTION/setup/setup_vm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install \
+	git \
+	vim \
+	make \
+	systemd \
+	ssh \
+	net-tools \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+exit 0
