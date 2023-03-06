b216882e42814bd0b496b60e5224dfc7430617af
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-03-06 13:53:37 +0900

    fix: Check openssl setting

--- INCEPTION/srcs/requirements/nginx/tools/entrypoint.sh
+++ INCEPTION/srcs/requirements/nginx/tools/entrypoint.sh
@@ -3,16 +3,23 @@
 # Exit if any command fails
 set -ex
 
-# OpenSSL setting
-mkdir openssl
+# Check whether openssl is already setup or not
+if [ ! -d /etc/nginx/openssl ]
+then
+	# OpenSSL setting
+	mkdir /etc/nginx/openssl
 
-openssl req -x509 -newkey rsa:4096 -nodes \
-	-out /openssl/sokim.42.fr.crt \
-	-keyout /openssl/sokim.42.fr.key \
-	-subj "/C=KR/ST=Seoul/L=Gaepo/O=InnovationAcademy/OU=42Seoul/CN=sokim.42.fr"
+	openssl req -x509 -newkey rsa:4096 -nodes \
+		-out /etc/nginx/openssl/sokim.42.fr.crt \
+		-keyout /etc/nginx/openssl/sokim.42.fr.key \
+		-subj "/C=KR/ST=Seoul/L=Gaepo/O=InnovationAcademy/OU=42Seoul/CN=sokim.42.fr"
 
 # NGINX configuration validation
-nginx -t
+	nginx -t
+	echo "SSL setting has done!"
+else
+	echo "OpenSSL already exists."
+fi
 
 # Run nginx server as foreground process
 exec "$@"
